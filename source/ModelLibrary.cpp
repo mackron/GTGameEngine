19fbfc1f20b067845fdbdfae866202b4e84645dc
Dave Reid <davidreid@galicktech.com.au>
2012-05-09 07:05:48 +1000

    Removed the old Collada loader.

--- source/ModelLibrary.cpp
+++ source/ModelLibrary.cpp
@@ -1,7 +1,6 @@
 
 #include <GTEngine/ModelLibrary.hpp>
 #include <GTEngine/VertexArrayFactory.hpp>
-#include <GTEngine/ColladaLoader.hpp>
 #include <GTEngine/SceneNode.hpp>
 #include <GTEngine/Components/ModelComponent.hpp>
 #include <GTEngine/Errors.hpp>
@@ -162,29 +161,6 @@ namespace GTEngine
         return nullptr;
     }
 
-    Model* ModelLibrary::LoadFromCollada(const char* filename)
-    {
-        Model* model = nullptr;
-
-        // We use the collada loader to load the file. This will construct a scene node containing all of the nodes in
-        // the collada file. All we care about is the first model component.
-        auto colladaNode = GTEngine::ColladaLoader::Load(filename);
-        if (colladaNode != nullptr)
-        {
-            // We just find the first node with a model component and strip it from the collada scene node, which we'll be discarding. We
-            // may need to tweak this is the player mode gets more complex than a single model.
-            auto modelNode = colladaNode->FindFirstChildWithComponent<GTEngine::ModelComponent>(true);
-            if (modelNode != nullptr)
-            {
-                // Now that we have the model node, we will steal the model from the model component.
-                model = modelNode->GetComponent<GTEngine::ModelComponent>()->GetModel();
-                model->ApplyTransformation(modelNode->GetTransformMatrix());
-            }
-        }
-
-        delete colladaNode;
-        return model;
-    }
 
     Model* ModelLibrary::CreatePlaneXZ(float width, float height, VertexFormat &format)
     {
