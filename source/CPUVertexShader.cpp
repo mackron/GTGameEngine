2bb28b18c6480f714fb60d3858701fd6e94bf48f
Dave Reid <davidreid@galicktech.com.au>
2012-05-15 23:06:44 +1000

    Implemented a few improvements to the CPU vertex shader. Shaders now have direct access to Position, TexCoord, Normal, Tangent and Bitangent vertex attributes as vec4's. This avoids the need to call Get() and Set() inside ProcessVertex(). It also just makes it easier for a shaders to gain access to those attributes.
    During this commit I experimented with GLM's SIMD vec4 and mat4 classes. That code remains in commented form. The SIMD experiment works very nicely in debug mode, but didn't have any noticeable difference in release mode. This may indeed be due to bottlenecks elsewhere. More thorough testing is needed.

--- source/CPUVertexShader.cpp
+++ source/CPUVertexShader.cpp
@@ -1,6 +1,101 @@
 
 #include <GTEngine/CPUVertexShader.hpp>
 
+// Helpers
+namespace GTEngine
+{
+    inline glm::vec4 GetVertexAttribute4(const float* data, size_t componentCount, size_t offset)
+    {
+        if (data != nullptr)
+        {
+            data += offset;
+
+            // We'll try and be clever here and predict the most common scenarios and put them first.
+            if (componentCount == 3)
+            {
+                return glm::vec4(data[0], data[1], data[2], 0.0f);
+            }
+            else if (componentCount == 2)
+            {
+                return glm::vec4(data[0], data[1], 0.0f, 0.0f);
+            }
+            else if (componentCount == 4)
+            {
+                return glm::vec4(data[0], data[1], data[2], data[3]);
+            }
+            else if (componentCount == 1)
+            {
+                return glm::vec4(data[0], 0.0f, 0.0f, 0.0f);
+            }
+            else
+            {
+                return glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
+            }
+        }
+        else
+        {
+            return glm::vec4();
+        }
+    }
+
+    inline void SetVertexAttribute4(float* data, size_t componentCount, size_t offset, const glm::vec4 &value)
+    {
+        if (data != nullptr)
+        {
+            data += offset;
+
+            // As above, we'll try and be clever here and predict the most common scenarios and put them first.
+            if (componentCount == 3)
+            {
+                data[0] = value.x;
+                data[1] = value.y;
+                data[2] = value.z;
+            }
+            else if (componentCount == 2)
+            {
+                data[0] = value.x;
+                data[1] = value.y;
+            }
+            else if (componentCount == 4)
+            {
+                data[0] = value.x;
+                data[1] = value.y;
+                data[2] = value.z;
+                data[3] = value.w;
+            }
+            else if (componentCount == 1)
+            {
+                data[0] = value.x;
+            }
+
+            /*
+            if (componentCount == 3)
+            {
+                data[0] = value.Data.m128_f32[0];
+                data[1] = value.Data.m128_f32[1];
+                data[2] = value.Data.m128_f32[2];
+            }
+            else if (componentCount == 2)
+            {
+                data[0] = value.Data.m128_f32[0];
+                data[1] = value.Data.m128_f32[1];
+            }
+            else if (componentCount == 4)
+            {
+                data[0] = value.Data.m128_f32[0];
+                data[1] = value.Data.m128_f32[1];
+                data[2] = value.Data.m128_f32[2];
+                data[3] = value.Data.m128_f32[3];
+            }
+            else if (componentCount == 1)
+            {
+                data[0] = value.Data.m128_f32[0];
+            }
+            */
+        }
+    }
+}
+
 namespace GTEngine
 {
     CPUVertexShader::CPUVertexShader()
@@ -22,17 +117,51 @@ namespace GTEngine
             this->vertexSizeInFloats = this->format.GetSize();
             this->output             = output;
 
+            size_t positionComponentCount;
+            size_t positionOffset;
+            bool loadPosition = this->format.GetAttributeInfo(VertexAttribs::Position, positionComponentCount, positionOffset);
+
+            size_t texCoordComponentCount;
+            size_t texCoordOffset;
+            bool loadTexCoord = this->format.GetAttributeInfo(VertexAttribs::TexCoord, texCoordComponentCount, texCoordOffset);
+
+            size_t normalComponentCount;
+            size_t normalOffset;
+            bool loadNormal = this->format.GetAttributeInfo(VertexAttribs::Normal, normalComponentCount, normalOffset);
+
+            size_t tangentComponentCount;
+            size_t tangentOffset;
+            bool loadTangent = this->format.GetAttributeInfo(VertexAttribs::Tangent, tangentComponentCount, tangentOffset);
+
+            size_t bitangentComponentCount;
+            size_t bitangentOffset;
+            bool loadBitangent = this->format.GetAttributeInfo(VertexAttribs::Bitangent, bitangentComponentCount, bitangentOffset);
+
+
+
             // We need to iterate over each vertex and process it.
             for (size_t i = 0; i < this->vertexCount; ++i)
             {
                 // The first step is to copy the vertex data to the output buffer.
                 auto vertexOutput = this->output + (i * this->vertexSizeInFloats);
                 auto vertexInput  = this->input  + (i * this->vertexSizeInFloats);
-                memcpy(vertexOutput, vertexInput, this->vertexSizeInFloats * sizeof(float));
+                //memcpy(vertexOutput, vertexInput, this->vertexSizeInFloats * sizeof(float));
 
                 // Now we can process the vertex.
                 Vertex vertex(i, vertexOutput, this->format);
+                if (loadPosition)  vertex.Position  = GetVertexAttribute4(vertexInput, positionComponentCount,  positionOffset);
+                if (loadTexCoord)  vertex.TexCoord  = GetVertexAttribute4(vertexInput, texCoordComponentCount,  texCoordOffset);
+                if (loadNormal)    vertex.Normal    = GetVertexAttribute4(vertexInput, normalComponentCount,    normalOffset);
+                if (loadTangent)   vertex.Tangent   = GetVertexAttribute4(vertexInput, tangentComponentCount,   tangentOffset);
+                if (loadBitangent) vertex.Bitangent = GetVertexAttribute4(vertexInput, bitangentComponentCount, bitangentOffset);
+
                 this->ProcessVertex(vertex);
+
+                if (loadPosition)  SetVertexAttribute4(vertexOutput, positionComponentCount,  positionOffset,  vertex.Position);
+                if (loadTexCoord)  SetVertexAttribute4(vertexOutput, texCoordComponentCount,  texCoordOffset,  vertex.TexCoord);
+                if (loadNormal)    SetVertexAttribute4(vertexOutput, normalComponentCount,    normalOffset,    vertex.Normal);
+                if (loadTangent)   SetVertexAttribute4(vertexOutput, tangentComponentCount,   tangentOffset,   vertex.Tangent);
+                if (loadBitangent) SetVertexAttribute4(vertexOutput, bitangentComponentCount, bitangentOffset, vertex.Bitangent);
             }
 
             return true;
@@ -47,7 +176,12 @@ namespace GTEngine
 namespace GTEngine
 {
     CPUVertexShader::Vertex::Vertex(unsigned int id, float* data, const VertexFormat &format)
-        : id(id), data(data), format(format)
+        : id(id), data(data), format(format),
+          Position(0.0f, 0.0f, 0.0f, 1.0f),
+          TexCoord(),
+          Normal(),
+          Tangent(),
+          Bitangent()
     {
     }
 
@@ -68,30 +202,11 @@ namespace GTEngine
         size_t stride;
         if (this->format.GetAttributeInfo(attribute, componentCount, stride))
         {
-            auto data = this->data + stride;
-
-            if (componentCount > 0)
-            {
-                value.x = *(data + 0);
-
-                if (componentCount > 1)
-                {
-                    value.y = *(data + 1);
-
-                    if (componentCount > 2)
-                    {
-                        value.z = *(data + 2);
-
-                        if (componentCount > 3)
-                        {
-                            value.w = *(data + 3);
-                        }
-                    }
-                }
-            }
+            value = GetVertexAttribute4(this->data, componentCount, stride);
         }
 
-        return value;
+        return glm::vec4(value.x, value.y, value.z, value.w);
+        //return glm::vec4(value.Data.m128_f32[0], value.Data.m128_f32[1], value.Data.m128_f32[2], value.Data.m128_f32[3]);
     }
 
     void CPUVertexShader::Vertex::Set(int attribute, float value)
@@ -115,27 +230,7 @@ namespace GTEngine
         size_t stride;
         if (this->format.GetAttributeInfo(attribute, componentCount, stride))
         {
-            auto data = this->data + stride;
-
-            if (componentCount > 0)
-            {
-                *(data + 0) = value.x;
-
-                if (componentCount > 1)
-                {
-                    *(data + 1) = value.y;
-
-                    if (componentCount > 2)
-                    {
-                        *(data + 2) = value.z;
-
-                        if (componentCount > 3)
-                        {
-                            *(data + 3) = value.w;
-                        }
-                    }
-                }
-            }
+            SetVertexAttribute4(this->data, componentCount, stride, glm::vec4(value));
         }
     }
 }
