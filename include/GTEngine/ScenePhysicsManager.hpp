9ef3e8d56510528b1b850be6233d2e500b154689
Dave Reid <davidreid@galicktech.com.au>
2012-07-08 19:56:25 +1000

    Added ScenePhysicsManager for managing the physics of a scene. This is mostly just a wrapper around a physics world at the moment, but should be fleshed out a bit more later on. This will be best for intercepting operations.
    A good example use case for this may be to inherit off DefaultScenePhysicsHandler and intercept changes to the gravity for things like projectiles or whatnot.
    
    Another use case may be to intercept the Step() method for doing custom physics stuff at the same time as regular rigid-body physics.

---
+++ include/GTEngine/ScenePhysicsManager.hpp
@@ -0,0 +1,54 @@
+
+#ifndef __GTEngine_ScenePhysicsManager_hpp_
+#define __GTEngine_ScenePhysicsManager_hpp_
+
+#include "SceneObject.hpp"
+#include "Physics.hpp"
+
+namespace GTEngine
+{
+    /// Base class for managing the physics in a scene.
+    class ScenePhysicsManager
+    {
+    public:
+
+        /// Destructor.
+        virtual ~ScenePhysicsManager() {}
+
+
+        /// Adds a collision object.
+        virtual void AddCollisionObject(CollisionObject &object, short group, short mask) = 0;
+        virtual void AddRigidBody(RigidBody &body, short group, short mask) = 0;
+        virtual void AddGhostObject(GhostObject &object, short group, short mask) = 0;
+
+        /// Removes a collision object.
+        virtual void RemoveCollisionObject(CollisionObject &object) = 0;
+        virtual void RemoveRigidBody(RigidBody &object) = 0;
+        virtual void RemoveGhostObject(GhostObject &object) = 0;
+
+
+        /// Sets the gravity.
+        virtual void SetGravity(float x, float y, float z) = 0;
+
+        /// Retrieves the gravity.
+        virtual void GetGravity(float &x, float &y, float &z) const = 0;
+
+
+        /// Performs a ray test against the objects in the physics world.
+        virtual void RayTest(const glm::vec3 &rayStart, const glm::vec3 &rayEnd, btCollisionWorld::RayResultCallback &callback) const = 0;
+
+        /// Performs a contact test between the given object and every other object.
+        virtual void ContactTest(const CollisionObject &object, btCollisionWorld::ContactResultCallback &callback) const = 0;
+        virtual void ContactTest(const RigidBody       &object, btCollisionWorld::ContactResultCallback &callback) const = 0;
+        virtual void ContactTest(const GhostObject     &object, btCollisionWorld::ContactResultCallback &callback) const = 0;
+    
+
+        /// Performs the update step.
+        ///
+        /// @param deltaTimeInSeconds [in] The delta time in seconds (time between updates).
+        virtual void Step(double deltaTimeInSeconds) = 0;
+
+    };
+}
+
+#endif
---
+++ source/DefaultScenePhysicsManager.cpp
@@ -0,0 +1,78 @@
+
+#include <GTEngine/DefaultScenePhysicsManager.hpp>
+#include <GTEngine/SceneNode.hpp>
+
+namespace GTEngine
+{
+    DefaultScenePhysicsManager::DefaultScenePhysicsManager()
+        : world()
+    {
+    }
+
+    DefaultScenePhysicsManager::~DefaultScenePhysicsManager()
+    {
+    }
+
+
+    void DefaultScenePhysicsManager::AddCollisionObject(CollisionObject &object, short group, short mask)
+    {
+        this->world.AddCollisionObject(object, group, mask);
+    }
+    void DefaultScenePhysicsManager::AddRigidBody(RigidBody &body, short group, short mask)
+    {
+        this->world.AddRigidBody(body, group, mask);
+    }
+    void DefaultScenePhysicsManager::AddGhostObject(GhostObject &object, short group, short mask)
+    {
+        this->world.AddGhostObject(object, group, mask);
+    }
+
+
+    void DefaultScenePhysicsManager::RemoveCollisionObject(CollisionObject &object)
+    {
+        this->world.RemoveCollisionObject(object);
+    }
+    void DefaultScenePhysicsManager::RemoveRigidBody(RigidBody &object)
+    {
+        this->world.RemoveRigidBody(object);
+    }
+    void DefaultScenePhysicsManager::RemoveGhostObject(GhostObject &object)
+    {
+        this->world.RemoveGhostObject(object);
+    }
+
+
+    void DefaultScenePhysicsManager::SetGravity(float x, float y, float z)
+    {
+        this->world.SetGravity(x, y, z);
+    }
+    void DefaultScenePhysicsManager::GetGravity(float &x, float &y, float &z) const
+    {
+        this->world.GetGravity(x, y, z);
+    }
+
+
+    void DefaultScenePhysicsManager::RayTest(const glm::vec3 &rayStart, const glm::vec3 &rayEnd, btCollisionWorld::RayResultCallback &callback) const
+    {
+        this->world.RayTest(rayStart, rayEnd, callback);
+    }
+
+    void DefaultScenePhysicsManager::ContactTest(const CollisionObject &object, btCollisionWorld::ContactResultCallback &callback) const
+    {
+        this->world.ContactTest(object, callback);
+    }
+    void DefaultScenePhysicsManager::ContactTest(const RigidBody &object, btCollisionWorld::ContactResultCallback &callback) const
+    {
+        this->world.ContactTest(object, callback);
+    }
+    void DefaultScenePhysicsManager::ContactTest(const GhostObject &object, btCollisionWorld::ContactResultCallback &callback) const
+    {
+        this->world.ContactTest(object, callback);
+    }
+
+
+    void DefaultScenePhysicsManager::Step(double deltaTimeInSeconds)
+    {
+        this->world.Step(static_cast<btScalar>(deltaTimeInSeconds), 4);
+    }
+}
