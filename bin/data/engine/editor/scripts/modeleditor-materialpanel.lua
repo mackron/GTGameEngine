e664b0fd93102f4baab9e56ec0d0fab47348f3b4
Dave Reid <davidreid@galicktech.com.au>
2012-09-03 18:43:32 +1000

    Added drag-and-drop for asset files. At the moment it only works for assigning materials to models, but will be extended later.

--- bin/data/engine/editor/scripts/modeleditor-materialpanel.lua
+++ bin/data/engine/editor/scripts/modeleditor-materialpanel.lua
@@ -10,6 +10,8 @@ function GTGUI.Element:ModelEditor_MaterialPanel()
         
         new:OnKeyPressed(function(data)
             if data.key == GTGUI.Keys.Enter then
+                new:ApplyMaterial();
+            --[[
                 if Editor.ModelEditor.SetMaterial(new.index, new:GetText()) then
                     new:SetStyle("border-color", "#6a6a6a");
                 else
@@ -17,8 +19,28 @@ function GTGUI.Element:ModelEditor_MaterialPanel()
                 end
                 
                 Editor_TabBar:GetActiveTab():MarkAsModified();
+                ]]
             end
         end)
+        
+        new:OnDrop(function(data)
+            if data.droppedElement.isAsset then
+                new:SetText(data.droppedElement.path);
+                new:ApplyMaterial();
+            end
+        end)
+        
+        
+        function new:ApplyMaterial()
+            if Editor.ModelEditor.SetMaterial(self.index, self:GetText()) then
+                self:SetStyle("border-color", "#6a6a6a");
+            else
+                self:SetStyle("border-color", "#cc6a6a");
+            end
+            
+            Editor_TabBar:GetActiveTab():MarkAsModified();
+        end
+        
         
         self.MaterialTextBoxes[new.index] = new;
     end
