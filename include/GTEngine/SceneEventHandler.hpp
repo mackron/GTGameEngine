02c6088b2a467beb8f639ec543511c37597075c0
Dave Reid <davidreid@galicktech.com.au>
2012-12-28 10:19:20 +1000

    Add the beginning of the SceneEventHandler class. This is used to allow stuff to attach an event handler to a scene object and then do stuff in response to certain things. The scene editor will be the first thing to use this class.

---
+++ include/GTEngine/SceneEventHandler.hpp
@@ -0,0 +1,44 @@
+
+#ifndef __GTEngine_SceneEventHandler_hpp_
+#define __GTEngine_SceneEventHandler_hpp_
+
+namespace GTEngine
+{
+    class Scene;
+    class SceneObject;
+
+    /// Class for intercepting events from a scene.
+    ///
+    /// This is really useful for special cases where you might need to do things in response to such things as adding or removing scene nodes. A prime
+    /// example of this is editors, where you might want to add metadata to an object as it is added to the editor. The engine's official scene editor
+    /// does exactly this.
+    class SceneEventHandler
+    {
+    public:
+
+        /// Constructor.
+        SceneEventHandler();
+
+        /// Destructor.
+        ~SceneEventHandler();
+
+
+        /// Called when a scene object is added to the scene.
+        ///
+        /// @param object [in] A reference to the object that was just added.
+        virtual void OnSceneObjectAdded(SceneObject &object);
+
+        /// Called when a scene object is being removed from the scene.
+        ///
+        /// @param object [in] A reference to the object that is being removed.
+        virtual void OnSceneObjectRemoved(SceneObject &object);
+
+
+    private:
+
+
+
+    };
+}
+
+#endif
---
+++ source/SceneEventHandler.cpp
@@ -0,0 +1,21 @@
+
+#include <GTEngine/SceneEventHandler.hpp>
+
+namespace GTEngine
+{
+    SceneEventHandler::SceneEventHandler()
+    {
+    }
+
+    SceneEventHandler::~SceneEventHandler()
+    {
+    }
+
+    void SceneEventHandler::OnSceneObjectAdded(SceneObject &)
+    {
+    }
+
+    void SceneEventHandler::OnSceneObjectRemoved(SceneObject &)
+    {
+    }
+}
