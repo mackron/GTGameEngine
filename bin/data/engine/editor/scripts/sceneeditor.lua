fb173dcf7f74e9868cdeba110cfb07231b1bb1e3
Dave Reid <davidreid@galicktech.com.au>
2012-12-28 19:26:52 +1000

    Add placeholder panels to the main panel of the scene editor.

--- bin/data/engine/editor/scripts/sceneeditor.lua
+++ bin/data/engine/editor/scripts/sceneeditor.lua
@@ -1,4 +1,68 @@
 
+function GTGUI.Element:Vector3Input()
+    self.X = GTGUI.Server.New("<div parentid='" .. self:GetID() .. "' styleclass='textbox' style='width:32%; margin-right:2px;' />");
+    self.Y = GTGUI.Server.New("<div parentid='" .. self:GetID() .. "' styleclass='textbox' style='width:32%; margin-right:2px;' />");
+    self.Z = GTGUI.Server.New("<div parentid='" .. self:GetID() .. "' styleclass='textbox' style='width:32%; margin-right:0px;' />");
+    
+    return self;
+end
+
+
+function GTGUI.Element:SceneEditorDetailsPanel()
+    self:PanelGroupBox("Details");
+    
+    self.NameContainer = GTGUI.Server.New("<div parentid='" .. self.Body:GetID()          .. "' style='width:100%; height:auto; child-plane:horizontal; flex-child-width:true; vertical-align:center;' />");
+    self.NameLabel     = GTGUI.Server.New("<div parentid='" .. self.NameContainer:GetID() .. "' style='width:auto; margin-right:4px; text-color:std-text-color;'>Name:</div>");
+    self.NameTextBox   = GTGUI.Server.New("<div parentid='" .. self.NameContainer:GetID() .. "' styleclass='textbox' style='width:100%;' />");
+end
+
+function GTGUI.Element:SceneEditorTransformPanel()
+    self:PanelGroupBox("Transform");
+    
+    self.Container = GTGUI.Server.New("<div parentid='" .. self:GetID()           .. "' style='width:100%; height:auto; child-plane:horizontal; flex-child-width:true;' />");
+    self.Left      = GTGUI.Server.New("<div parentid='" .. self.Container:GetID() .. "' style='width:auto; height:auto; margin-right:4px;' />");
+    self.Right     = GTGUI.Server.New("<div parentid='" .. self.Container:GetID() .. "' style='width:100%; height:auto;' />");
+    
+    self.PositionLabel = GTGUI.Server.New("<div parentid='" .. self.Left:GetID()  .. "' style='width:auto; text-color:std-text-color; padding:0px 3px; margin-bottom:4px;'>Position:</div>");
+    self.RotationLabel = GTGUI.Server.New("<div parentid='" .. self.Left:GetID()  .. "' style='width:auto; text-color:std-text-color; padding:0px 3px; margin-bottom:4px;'>Rotation:</div>");
+    self.ScaleLabel    = GTGUI.Server.New("<div parentid='" .. self.Left:GetID()  .. "' style='width:auto; text-color:std-text-color; padding:0px 3px;'>Scale:</div>");
+    
+    self.PositionInput = GTGUI.Server.New("<div parentid='" .. self.Right:GetID() .. "' style='width:100%; height:auto; horizontal-align:right; child-plane:horizontal; flex-child-width:true; margin-bottom:4px;' />"):Vector3Input();
+    self.RotationInput = GTGUI.Server.New("<div parentid='" .. self.Right:GetID() .. "' style='width:100%; height:auto; horizontal-align:right; child-plane:horizontal; flex-child-width:true; margin-bottom:4px;' />"):Vector3Input();
+    self.ScaleInput    = GTGUI.Server.New("<div parentid='" .. self.Right:GetID() .. "' style='width:100%; height:auto; horizontal-align:right; child-plane:horizontal; flex-child-width:true; margin-bottom:4px;' />"):Vector3Input();
+end
+
+
+function GTGUI.Element:SceneEditorPanel()
+    self.Body      = GTGUI.Server.New("<div parentid='" .. self:GetID() .. "' styleclass='scene-editor-panel-body' style='' />");
+    self.Scrollbar = GTGUI.Server.New("<div parentid='" .. self:GetID() .. "' styleclass='vertical-scrollbar'      style='' />");
+    
+    
+    self.Body.DetailsPanel = GTGUI.Server.New("<div parentid='" .. self.Body:GetID() .. "' styleclass='panel-groupbox' style='' />");
+    self.Body.DetailsPanel:SceneEditorDetailsPanel();
+    GTGUI.Server.New("<div parentid='" .. self.Body:GetID() .. "' styleclass='panel-groupbox-separator' style='' />");
+    self.Body.TransformPanel = GTGUI.Server.New("<div parentid='" .. self.Body:GetID() .. "' styleclass='panel-groupbox' style='' />");
+    self.Body.TransformPanel:SceneEditorTransformPanel();
+    
+
+    self:OnSize(function()
+        self.Scrollbar:SetPageSize(self:GetHeight());
+    end);
+    
+    self.Body:OnSize(function()
+        self.Scrollbar:SetRange(self.Body:GetHeight());
+    end)
+    
+    self.Scrollbar:VerticalScrollbar();
+    self.Scrollbar:SetPageSize(self:GetHeight());
+    self.Scrollbar:EnableAutoVisibility();
+    self.Scrollbar:OnScroll(function(data)
+        local offset = -data.position;
+        self.Body:SetStyle("inner-offset-y", tostring(offset));
+    end)
+end
+
+
 function GTGUI.Element:SceneEditor()
     self.Viewport    = GTGUI.Server.New("<div parentid='" .. self:GetID() .. "' styleclass='scene-editor-viewport' style='' />");
     self.Panel       = GTGUI.Server.New("<div parentid='" .. self:GetID() .. "' styleclass='scene-editor-panel'    style='' />");
@@ -10,6 +74,7 @@ function GTGUI.Element:SceneEditor()
     self.HasMouseCapture         = false;
     
     
+    self.Panel:SceneEditorPanel();
     self.ContextMenu:Menu();
     
     self.ContextMenu:AppendItem("Add Cube"):OnPressed(function()
