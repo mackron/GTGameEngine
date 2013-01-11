1bb1b8cf6dac4c4d0c576eb09e72f5a310ec3215
Dave Reid <mackron@gmail.com>
2013-01-12 09:05:03 +1000

    The first of many commits completely reworking the way the sub-editors work in the main editor.

---
+++ include/GTEngine/Editor/SceneEditor/SceneEditor.hpp
@@ -0,0 +1,42 @@
+
+#ifndef __GTEngine_SceneEditor_hpp_
+#define __GTEngine_SceneEditor_hpp_
+
+#include "../SubEditor.hpp"
+
+namespace GTEngine
+{
+    /// Class representing a scene editor.
+    class SceneEditor : public SubEditor
+    {
+    public:
+
+        /// Constructor.
+        SceneEditor(const char* path, const char* relativeTo);
+
+        /// Destructor.
+        ~SceneEditor();
+
+
+
+        ///////////////////////////////////////////////////
+        // Virtual Methods.
+
+        /// SubEditor::Show()
+        void Show();
+
+        /// SubEditor::Hide()
+        void Hide();
+
+        /// SubEditor::Save()
+        void Save();
+
+
+
+    private:
+
+
+    };
+}
+
+#endif
