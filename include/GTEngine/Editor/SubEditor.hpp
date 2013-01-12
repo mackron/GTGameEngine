a48a944131a5b2674a13334463e92dd666f0f660
Dave Reid <mackron@gmail.com>
2013-01-12 10:43:05 +1000

    Make a few improvements to the constructor or sub-editors.

--- include/GTEngine/Editor/SubEditor.hpp
+++ include/GTEngine/Editor/SubEditor.hpp
@@ -6,6 +6,8 @@
 
 namespace GTEngine
 {
+    class Editor;
+
     /// Base class for sub-editors.
     ///
     /// A sub-editor is the scene editor, model editor, text editor, etc. There is one sub-editor associated with every open file in the editor.
@@ -16,10 +18,15 @@ namespace GTEngine
     public:
 
         /// Constructor.
-        SubEditor(const char* path, const char* relativeTo);
+        SubEditor(Editor &ownerEditor, const char* absolutePath, const char* relativePath);
 
         /// Destructor.
         virtual ~SubEditor();
+
+        
+        /// Retrieves a reference to the owner editor.
+              Editor & GetOwnerEditor()       { return this->ownerEditor; }
+        const Editor & GetOwnerEditor() const { return this->ownerEditor; }
 
 
         /// Retrieves the absolute path of this editor's file.
@@ -46,11 +53,19 @@ namespace GTEngine
 
     private:
 
+        /// A reference to the Editor object that owns this sub-editor.
+        Editor &ownerEditor;
+
         /// The absolute path of the file this editor is editting.
         GTCore::String absolutePath;
 
         /// The relative path of the file this editor is editting.
         GTCore::String relativePath;
+
+
+    private:    // No copying.
+        SubEditor(const SubEditor &);
+        SubEditor & operator=(const SubEditor &);
     };
 }
 
