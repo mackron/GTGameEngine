37d550ece92716418804d2ea897284c3e71d869d
Dave Reid <mackron@gmail.com>
2013-01-13 11:27:18 +1000

    Implement the new image editor.

--- include/GTEngine/Editor/ImageEditor/ImageEditor.hpp
+++ include/GTEngine/Editor/ImageEditor/ImageEditor.hpp
@@ -3,6 +3,7 @@
 #define __GTEngine_ImageEditor_hpp_
 
 #include "../SubEditor.hpp"
+#include "../../Texture2DLibrary.hpp"
 
 namespace GTEngine
 {
@@ -17,6 +18,18 @@ namespace GTEngine
         /// Destructor.
         ~ImageEditor();
 
+
+
+        /// Retrieves a pointer to the Texture2D image.
+              Texture2D* GetImage()       { return this->image; }
+        const Texture2D* GetImage() const { return this->image; }
+
+
+        /// Retrieves the current zoom.
+        float GetZoom() const { return this->zoom; }
+
+        /// Sets the current zoom.
+        void SetZoom(float newZoom) { this->zoom = newZoom; }
 
 
         ///////////////////////////////////////////////////
@@ -34,7 +47,46 @@ namespace GTEngine
 
     private:
 
+        /// The viewport the image will be drawn to.
+        GTGUI::Element* viewportElement;
 
+        /// The event handler for the viewport. We need this for the mouse wheel and drawing.
+        /// Event handler for the viewport. Mainly needed for drawing the image at draw time.
+        struct ViewportEventHandler : public GTGUI::ElementEventHandler
+        {
+            /// Constructor.
+            ViewportEventHandler(ImageEditor &ownerEditorIn)
+                : ownerEditor(ownerEditorIn)
+            {
+            }
+
+
+            /// OnDraw.
+            void OnDraw(GTGUI::Element &element);
+
+            /// OnMouseWheel.
+            void OnMouseWheel(GTGUI::Element &element, int delta, int x, int y);
+
+
+        private:
+
+            /// The editor that owns this viewport.
+            ImageEditor &ownerEditor;
+
+
+        private:    // No copying.
+            ViewportEventHandler(const ViewportEventHandler &);
+            ViewportEventHandler & operator=(const ViewportEventHandler &);
+
+        }viewportEventHandler;
+
+
+
+        /// The texture to draw.
+        Texture2D* image;
+
+        /// The zoom level.
+        float zoom;
     };
 }
 
--- source/Editor/ImageEditor/ImageEditor.cpp
+++ source/Editor/ImageEditor/ImageEditor.cpp
@@ -1,15 +1,35 @@
 
 #include <GTEngine/Editor/ImageEditor/ImageEditor.hpp>
+#include <GTEngine/Rendering/Renderer.hpp>
+#include <GTEngine/ShaderLibrary.hpp>
+
+#if defined(_MSC_VER)
+    #pragma warning(push)
+    #pragma warning(disable:4355)   // 'this' used in initialise list.
+#endif
 
 namespace GTEngine
 {
     ImageEditor::ImageEditor(Editor &ownerEditor, const char* absolutePath, const char* relativePath)
-        : SubEditor(ownerEditor, absolutePath, relativePath)
+        : SubEditor(ownerEditor, absolutePath, relativePath),
+          viewportElement(nullptr), viewportEventHandler(*this),
+          image(Texture2DLibrary::Acquire(absolutePath, relativePath)), zoom(1.0f)
     {
+        if (this->image != nullptr)
+        {
+            auto &gui = this->GetGUI();
+
+            this->viewportElement = gui.CreateElement("<div parentid='Editor_SubEditorContainer' style='width:100%; height:100%; border:1px #222;' />");
+            assert(this->viewportElement != nullptr);
+            {
+                this->viewportElement->AttachEventHandler(this->viewportEventHandler);
+            }
+        }
     }
 
     ImageEditor::~ImageEditor()
     {
+        Texture2DLibrary::Unacquire(this->image);
     }
 
 
@@ -19,14 +39,84 @@ namespace GTEngine
 
     void ImageEditor::Show()
     {
+        this->viewportElement->Show();
     }
 
     void ImageEditor::Hide()
     {
+        this->viewportElement->Hide();
     }
 
     bool ImageEditor::Save()
     {
         return true;
     }
+
+
+
+
+    //////////////////////////////////////////////////////
+    // ViewportEventHandler
+
+    void ImageEditor::ViewportEventHandler::OnDraw(GTGUI::Element &element)
+    {
+        auto image = this->ownerEditor.GetImage();
+        if (image != nullptr)
+        {
+            Renderer::SetShader(GTEngine::ShaderLibrary::GetTextured2DQuadShader());
+            Renderer::SetShaderParameter("Projection", glm::ortho(0.0f, static_cast<float>(element.server.GetViewportWidth()), static_cast<float>(element.server.GetViewportHeight()), 0.0f, 0.0f, -1.0f));
+            Renderer::SetShaderParameter("Texture",    image);
+            
+
+            unsigned int imageWidth  = image->GetWidth();
+            unsigned int imageHeight = image->GetHeight();
+
+            GTGUI::Rect viewportRect;
+            element.GetAbsoluteRect(viewportRect);
+
+            float viewportWidth  = static_cast<float>(viewportRect.right  - viewportRect.left);
+            float viewportHeight = static_cast<float>(viewportRect.bottom - viewportRect.top);
+
+            float zoom = this->ownerEditor.GetZoom();
+
+            float quadLeft   = (viewportWidth  * 0.5f) - (imageWidth  * 0.5f * zoom) + viewportRect.left;
+            float quadRight  = (viewportWidth  * 0.5f) + (imageWidth  * 0.5f * zoom) + viewportRect.left;
+            float quadTop    = (viewportHeight * 0.5f) - (imageHeight * 0.5f * zoom) + viewportRect.top;
+            float quadBottom = (viewportHeight * 0.5f) + (imageHeight * 0.5f * zoom) + viewportRect.top;
+
+
+
+            float quadVertices[] =
+            {
+                quadLeft,  quadBottom,
+                0.0f,      0.0f,
+
+                quadRight, quadBottom,
+                1.0f,      0.0f,
+
+                quadRight, quadTop,
+                1.0f,      1.0f,
+
+                quadLeft,  quadTop,
+                0.0f,      1.0f
+            };
+
+            unsigned int quadIndices[] =
+            {
+                0, 1, 2,
+                2, 3, 0,
+            };
+
+            Renderer::Draw(quadVertices, quadIndices, 6, VertexFormat::P2T2);
+        }
+    }
+
+    void ImageEditor::ViewportEventHandler::OnMouseWheel(GTGUI::Element &, int delta, int, int)
+    {
+        this->ownerEditor.SetZoom(this->ownerEditor.GetZoom() + (this->ownerEditor.GetZoom() * (0.1f * delta)));
+    }
 }
+
+#if defined(_MSC_VER)
+    #pragma warning(pop)
+#endif
