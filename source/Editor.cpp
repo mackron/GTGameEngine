935373cb5a700df2fe8c282de731c72de5d05651
Dave Reid <mackron@gmail.com>
2013-01-15 08:52:33 +1000

    More dead code cleanup.

--- source/Editor.cpp
+++ source/Editor.cpp
@@ -20,7 +20,6 @@ namespace GTEngine
         : game(game),
           openedFiles(), currentlyShownEditor(nullptr),
           GUI(),
-          //modelEditor(*this), imageEditor(*this), textEditor(*this), sceneEditor(*this),
           lastProfilingUpdateTime(0.0),
           isStarted(false), isOpen(false), disableFileWatchingAfterClose(true), disableKeyboardAutoRepeatAfterClose(true),
           dataFilesWatcherEventHandler(*this)
@@ -139,13 +138,6 @@ namespace GTEngine
                 // We actually want the editor element to be the first child of the root. If we don't do this, sometimes a game can be in a state
                 // where the editor will be placed underneath another GUI element, causing it to not look quite right.
                 guiServer.GetRootElement()->PrependChild(*this->GUI.EditorMain);
-
-                // Here we startup our sub-editors.
-                //this->modelEditor.Startup();
-                //this->imageEditor.Startup();
-                //this->soundEditor.Startup();
-                //this->textEditor.Startup();
-                //this->sceneEditor.Startup();
 
 
                 // Here we need to attach our files watcher event handler.
@@ -757,111 +749,6 @@ namespace GTEngine
     }
 
 
-    /*
-    void Editor::OnModelActivated(const char* fileName, const char* makeRelativeTo)
-    {
-        this->modelEditor.LoadModel(fileName, makeRelativeTo);
-    }
-
-    void Editor::OnImageActivated(const char* fileName, const char* makeRelativeTo)
-    {
-        this->imageEditor.LoadImage(fileName, makeRelativeTo);
-    }
-
-    void Editor::OnSoundActivated(const char* fileName, const char* makeRelativeTo)
-    {
-        (void)fileName;
-        (void)makeRelativeTo;
-    }
-
-    void Editor::OnTextFileActivated(const char* fileName, const char* makeRelativeTo)
-    {
-        this->textEditor.LoadFile(fileName, makeRelativeTo);
-    }
-
-    void Editor::OnSceneActivated(const char* fileName, const char* makeRelativeTo)
-    {
-        this->sceneEditor.LoadScene(fileName, makeRelativeTo);
-    }
-
-
-
-    void Editor::OnModelClosed(const char* fileName)
-    {
-        (void)fileName;
-    }
-
-    void Editor::OnImageClosed(const char* fileName)
-    {
-        (void)fileName;
-    }
-
-    void Editor::OnSoundClosed(const char* fileName)
-    {
-        (void)fileName;
-    }
-
-    void Editor::OnTextFileClosed(const char* fileName)
-    {
-        this->textEditor.CloseFile(fileName);
-    }
-
-    void Editor::OnSceneClosed(const char* fileName)
-    {
-        this->sceneEditor.CloseScene(fileName);
-    }
-
-
-
-    void Editor::OnFileActivated(const char* fileName, const char* makeRelativeTo)
-    {
-        if (IO::IsSupportedModelExtension(fileName))
-        {
-            this->OnModelActivated(fileName, makeRelativeTo);
-        }
-        else if (IO::IsSupportedImageExtension(fileName))
-        {
-            this->OnImageActivated(fileName, makeRelativeTo);
-        }
-        else if (IO::IsSupportedSoundExtension(fileName))
-        {
-            this->OnSoundActivated(fileName, makeRelativeTo);
-        }
-        else if (IO::IsSupportedSceneExtension(fileName))
-        {
-            this->OnSceneActivated(fileName, makeRelativeTo);
-        }
-        else        // Assume a text file if nothing else.
-        {
-            this->OnTextFileActivated(fileName, makeRelativeTo);
-        }
-    }
-
-    void Editor::OnFileClosed(const char* fileName)
-    {
-        if (IO::IsSupportedModelExtension(fileName))
-        {
-            this->OnModelClosed(fileName);
-        }
-        else if (IO::IsSupportedImageExtension(fileName))
-        {
-            this->OnImageClosed(fileName);
-        }
-        else if (IO::IsSupportedSoundExtension(fileName))
-        {
-            this->OnSoundClosed(fileName);
-        }
-        else if (IO::IsSupportedSceneExtension(fileName))
-        {
-            this->OnSceneClosed(fileName);
-        }
-        else        // Assume a text file if nothing else.
-        {
-            this->OnTextFileClosed(fileName);
-        }
-    }
-    */
-
     // FIXME: There's a bug here where if a file path includes double quotes, the script will fail to execute.
     void Editor::OnFileInsert(const DataFilesWatcher::Item &item)
     {
@@ -989,46 +876,6 @@ namespace GTEngine
             script.SetTableFunction(-1, "MarkFileAsModified",       FFI::MarkFileAsModified);
             script.SetTableFunction(-1, "UnmarkFileAsModified",     FFI::UnmarkFileAsModified);
             script.SetTableFunction(-1, "IsFileMarkedAsModified",   FFI::IsFileMarkedAsModified);
-
-            /*
-            script.SetTableFunction(-1, "OnModelActivated",         FFI::OnModelActivated);
-            script.SetTableFunction(-1, "OnImageActivated",         FFI::OnImageActivated);
-            script.SetTableFunction(-1, "OnSoundActivated",         FFI::OnSoundActivated);
-            script.SetTableFunction(-1, "OnTextFileActivated",      FFI::OnTextFileActivated);
-            script.SetTableFunction(-1, "OnSceneActivated",         FFI::OnSceneActivated);
-
-            script.SetTableFunction(-1, "OnFileActivated",          FFI::OnFileActivated);
-            script.SetTableFunction(-1, "OnFileClosed",             FFI::OnFileClosed);
-            */
-
-            /*
-            script.Push("ModelEditor");
-            script.GetTableValue(-2);
-            if (script.IsTable(-1))
-            {
-                script.SetTableFunction(-1, "SaveFile", FFI::ModelEditorFFI::SaveFile);
-
-                script.SetTableFunction(-1, "SetMaterial", FFI::ModelEditorFFI::SetMaterial);
-
-                script.SetTableFunction(-1, "ShowConvexDecomposition",  FFI::ModelEditorFFI::ShowConvexDecomposition);
-                script.SetTableFunction(-1, "HideConvexDecomposition",  FFI::ModelEditorFFI::HideConvexDecomposition);
-                script.SetTableFunction(-1, "BuildConvexDecomposition", FFI::ModelEditorFFI::BuildConvexDecomposition);
-
-                script.SetTableFunction(-1, "PlayAnimation", FFI::ModelEditorFFI::PlayAnimation);
-                script.SetTableFunction(-1, "StopAnimation", FFI::ModelEditorFFI::StopAnimation);
-            }
-            script.Pop(1);
-            */
-
-            /*
-            script.Push("TextEditor");
-            script.GetTableValue(-2);
-            if (script.IsTable(-1))
-            {
-                script.SetTableFunction(-1, "SaveFile", FFI::TextEditorFFI::SaveFile);
-            }
-            script.Pop(1);
-            */
         }
         script.Pop(1);
     }
@@ -1127,142 +974,6 @@ namespace GTEngine
         script.Push(FFI::GetEditor(script).IsFileMarkedAsModified(script.ToString(1), script.ToString(2)));
         return 1;
     }
-
-
-    /*
-    int Editor::FFI::OnModelActivated(GTCore::Script &script)
-    {
-        FFI::GetEditor(script).OnModelActivated(script.ToString(1), script.ToString(2));
-        return 0;
-    }
-
-    int Editor::FFI::OnImageActivated(GTCore::Script &script)
-    {
-        FFI::GetEditor(script).OnImageActivated(script.ToString(1), script.ToString(2));
-        return 0;
-    }
-
-    int Editor::FFI::OnSoundActivated(GTCore::Script &script)
-    {
-        FFI::GetEditor(script).OnSoundActivated(script.ToString(1), script.ToString(2));
-        return 0;
-    }
-
-    int Editor::FFI::OnTextFileActivated(GTCore::Script &script)
-    {
-        FFI::GetEditor(script).OnTextFileActivated(script.ToString(1), script.ToString(2));
-        return 0;
-    }
-
-    int Editor::FFI::OnSceneActivated(GTCore::Script &script)
-    {
-        FFI::GetEditor(script).OnSceneActivated(script.ToString(1), script.ToString(2));
-        return 0;
-    }
-
-
-
-    int Editor::FFI::OnFileActivated(GTCore::Script &script)
-    {
-        FFI::GetEditor(script).OnFileActivated(script.ToString(1), script.ToString(2));
-        return 0;
-    }
-
-    int Editor::FFI::OnFileClosed(GTCore::Script &script)
-    {
-        FFI::GetEditor(script).OnFileClosed(script.ToString(1));
-        return 0;
-    }
-    */
-
-
-    /*
-    // ModelEditor
-    int Editor::FFI::ModelEditorFFI::SaveFile(GTCore::Script &script)
-    {
-        script.Push(FFI::GetEditor(script).GetModelEditor().SaveModel(script.ToString(1)));
-        return 1;
-    }
-
-    int Editor::FFI::ModelEditorFFI::SetMaterial(GTCore::Script &script)
-    {
-        script.Push(FFI::GetEditor(script).GetModelEditor().SetMaterial(script.ToInteger(1) - 1, script.ToString(2)));  // -1 because we will have passed a 1-based index from Lua.
-        return 1;
-    }
-
-    int Editor::FFI::ModelEditorFFI::ShowConvexDecomposition(GTCore::Script &script)
-    {
-        auto &game = GameScript::FFI::GetGameObject(script);
-
-        game.GetEditor().GetModelEditor().ShowConvexDecomposition();
-        return 0;
-    }
-
-    int Editor::FFI::ModelEditorFFI::HideConvexDecomposition(GTCore::Script &script)
-    {
-        auto &game = GameScript::FFI::GetGameObject(script);
-
-        game.GetEditor().GetModelEditor().HideConvexDecomposition();
-        return 0;
-    }
-
-    int Editor::FFI::ModelEditorFFI::BuildConvexDecomposition(GTCore::Script &script)
-    {
-        auto &game = GameScript::FFI::GetGameObject(script);
-
-        ConvexHullBuildSettings settings;
-        settings.compacityWeight               = script.ToFloat(1);
-        settings.volumeWeight                  = script.ToFloat(2);
-        settings.minClusters                   = static_cast<unsigned int>(script.ToInteger(3));
-        settings.verticesPerCH                 = static_cast<unsigned int>(script.ToInteger(4));
-        settings.concavity                     = script.ToFloat(5);
-        settings.smallClusterThreshold         = script.ToFloat(6);
-        settings.connectedComponentsDist       = script.ToFloat(7);
-        settings.simplifiedTriangleCountTarget = static_cast<unsigned int>(script.ToInteger(8));
-        settings.addExtraDistPoints            = script.ToBoolean(9);
-        settings.addFacesPoints                = script.ToBoolean(10);
-
-        game.GetEditor().GetModelEditor().BuildConvexDecomposition(settings);
-        return 0;
-    }
-
-    int Editor::FFI::ModelEditorFFI::PlayAnimation(GTCore::Script &script)
-    {
-        auto &game = GameScript::FFI::GetGameObject(script);
-
-        game.GetEditor().GetModelEditor().PlayAnimation();
-        return 0;
-    }
-
-    int Editor::FFI::ModelEditorFFI::StopAnimation(GTCore::Script &script)
-    {
-        auto &game = GameScript::FFI::GetGameObject(script);
-
-        game.GetEditor().GetModelEditor().StopAnimation();
-        return 0;
-    }
-    */
-
-
-    ////////////////////////////////////////////////////
-    // TextEditorFFI
-    /*
-    int Editor::FFI::TextEditorFFI::SaveFile(GTCore::Script &script)
-    {
-        script.Push(FFI::GetEditor(script).GetTextEditor().SaveFile(script.ToString(1)));
-        return 1;
-    }
-    */
-
-    ////////////////////////////////////////////////////
-    // SceneEditorFFI
-    /*
-    int Editor::FFI::SceneEditorFFI::SaveFile(GTCore::Script &script)
-    {
-        script.Push(FFI::GetEditor(script).GetSceneEditor().SaveScene(script.ToString(1)));
-        return 1;
-    }
-    */
 }
 
 
