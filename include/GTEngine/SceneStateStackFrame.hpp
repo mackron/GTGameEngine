7e4d346f4b99756c4b3425f68ae1b4c272965741
Dave Reid <mackron@gmail.com>
2013-01-21 11:00:41 +1000

    Add empty framework for the new scene state stack system.

---
+++ include/GTEngine/SceneStateStackFrame.hpp
@@ -0,0 +1,25 @@
+
+#ifndef __GTEngine_SceneStateStackFrame_hpp_
+#define __GTEngine_SceneStateStackFrame_hpp_
+
+namespace GTEngine
+{
+    class SceneStateStackFrame
+    {
+    public:
+
+        /// Constructor.
+        SceneStateStackFrame();
+
+        /// Destructor.
+        ~SceneStateStackFrame();
+
+
+
+    private:
+
+
+    };
+}
+
+#endif
