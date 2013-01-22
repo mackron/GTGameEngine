d077085b9eb0808d39e91cde7d6b1f0532ecf525
Dave Reid <mackron@gmail.com>
2013-01-22 17:07:38 +1000

    More ground work on the scene state stack system.
    With this commit the branching foundations have been laid down. Everything is untested at the moment.

--- include/GTEngine/SceneStateStackBranch.hpp
+++ include/GTEngine/SceneStateStackBranch.hpp
@@ -12,13 +12,14 @@ namespace GTEngine
     ///
     /// A branch is usually a child to another branch, except for the master branch. A branch can also, of course, have multiple children.
     ///
-    /// A branch is created at a frame of another branch. 
+    /// A branch is created at a frame of another branch. Each branch has it's own set of frames, so a frame index on one brach will point
+    /// to a different frame for another branch.
     class SceneStateStackBranch
     {
     public:
 
         /// Constructor.
-        SceneStateStackBranch(SceneStateStackBranch* parent, uint32_t parentFrameIndex);
+        SceneStateStackBranch(SceneStateStackBranch* parent, uint32_t rootFrameIndex);
 
         /// Destructor.
         ~SceneStateStackBranch();
@@ -28,13 +29,67 @@ namespace GTEngine
               SceneStateStackBranch* GetParent()       { return this->parent; };
         const SceneStateStackBranch* GetParent() const { return this->parent; };
 
+        /// Re-parents the branch.
+        ///
+        /// @param newParent         [in] A pointer to the new parent branch.
+        /// @param newRootFrameIndex [in] The new root frame index.
+        ///
+        /// @remarks
+        ///     Re-parenting causes the local frames and every child branch to be deleted. The reason is that with a change in parent, the data
+        ///     contained in any existing frames will have become invalid.
+        void SetParent(SceneStateStackBranch* newParent, uint32_t newRootFrameIndex);
 
-        /// Retrieves the index of the frame of the parent branch that this branch begins from.
-        uint32_t GetParentFrameIndex() const { return this->parentFrameIndex; }
+
+        /// Retrieves the index of the frame this branch is rooted at.
+        uint32_t GetRootFrameIndex() const { return this->rootFrameIndex; }
+
+        /// Sets the root frame index.
+        void SetRootFrameIndex(uint32_t newRootFrameIndex) { this->rootFrameIndex = newRootFrameIndex; }
 
 
         /// Retrieves the index of the current frame.
         uint32_t GetCurrentFrameIndex() const { return this->currentFrameIndex; }
+
+
+
+        /// Creates and appends a child branch at the current frame index.
+        ///
+        /// @remarks
+        ///     This method asserts that the current frame index is pointing to a local frame.
+        SceneStateStackBranch* CreateBranch();
+
+        /// Deletes a child branch.
+        ///
+        /// @param childBranch [in] The child branch to delete.
+        void DeleteBranch(SceneStateStackBranch* childBranch);
+
+        /// Removes all child branches.
+        void DeleteAllBranches();
+
+
+        /// Appends a new frame to the branch at the position just after the current frame index.
+        ///
+        /// @remarks
+        ///     The new frame will become the current one.
+        ///     @par
+        ///     Everything coming after the current frame will be deleted, including child branches.
+        void AppendFrame();
+
+
+
+        /////////////////////////////////////////////
+        // Internal Use Only
+
+        /// Deletes a child branch by it's index.
+        ///
+        /// @param childIndex [in] The index of the child to remove.
+        void _DeleteBranchByIndex(size_t childIndex);
+
+
+    private:
+
+        /// Recursively finds the branch where the given frame index is local.
+        SceneStateStackBranch* FindBranch(uint32_t frameIndex);
 
 
     private:
@@ -42,8 +97,11 @@ namespace GTEngine
         /// The parent branch. This will be null if the branch does not have a parent (the master branch).
         SceneStateStackBranch* parent;
 
+        /// The list of children.
+        GTCore::Vector<SceneStateStackBranch*> children;
+
         /// The index of the frame of the parent branch that this branch begins from.
-        uint32_t parentFrameIndex;
+        uint32_t rootFrameIndex;
 
 
         /// The flat list of frames belonging to this branch.
--- source/SceneStateStackBranch.cpp
+++ source/SceneStateStackBranch.cpp
@@ -3,17 +3,157 @@
 
 namespace GTEngine
 {
-    SceneStateStackBranch::SceneStateStackBranch(SceneStateStackBranch* parentIn, uint32_t parentFrameIndexIn)
-        : parent(parentIn), parentFrameIndex(parentFrameIndexIn),
-          frames(), currentFrameIndex(0)
+    SceneStateStackBranch::SceneStateStackBranch(SceneStateStackBranch* parentIn, uint32_t rootFrameIndexIn)
+        : parent(parentIn), children(), rootFrameIndex(rootFrameIndexIn),
+          frames(), currentFrameIndex(rootFrameIndexIn)
     {
     }
 
     SceneStateStackBranch::~SceneStateStackBranch()
     {
+        this->DeleteAllBranches();
+
         for (size_t i = 0; i < this->frames.count; ++i)
         {
             delete this->frames[i];
         }
+    }
+
+
+    void SceneStateStackBranch::SetParent(SceneStateStackBranch* newParent, uint32_t newRootFrameIndex)
+    {
+        this->parent            = newParent;
+        this->rootFrameIndex    = newRootFrameIndex;
+        this->currentFrameIndex = newRootFrameIndex;
+
+
+        // Children and local frames need to be deleted.
+        this->DeleteAllBranches();
+
+
+        for (size_t i = 0; i < this->frames.count; ++i)
+        {
+            auto frame = this->frames[i];
+            assert(frame != nullptr);
+            {
+                delete frame;
+            }
+        }
+        this->frames.Clear();
+    }
+
+
+    SceneStateStackBranch* SceneStateStackBranch::CreateBranch()
+    {
+        assert(this->currentFrameIndex >  this->rootFrameIndex);
+        assert(this->currentFrameIndex <= this->rootFrameIndex + this->frames.count);
+        {
+            auto child = new SceneStateStackBranch(this, this->currentFrameIndex);
+            this->children.PushBack(child);
+
+            return child;
+        }
+    }
+
+    void SceneStateStackBranch::DeleteBranch(SceneStateStackBranch* childBranch)
+    {
+        size_t childIndex;
+        if (this->children.FindFirstIndexOf(childBranch, childIndex))
+        {
+            this->_DeleteBranchByIndex(childIndex);
+        }
+    }
+
+    void SceneStateStackBranch::DeleteAllBranches()
+    {
+        for (size_t i = 0; i < this->children.count; ++i)
+        {
+            delete this->children[i];
+        }
+        this->children.Clear();
+    }
+
+
+    void SceneStateStackBranch::AppendFrame()
+    {
+        if (this->currentFrameIndex < this->rootFrameIndex)
+        {
+            // We need to reparent the branch. The new parent will be the one where the current frame index is one of it's local frames.
+            auto newParent = this->FindBranch(this->currentFrameIndex);
+            assert(newParent != nullptr);
+            {
+                this->SetParent(newParent, this->currentFrameIndex);
+            }
+        }
+
+
+
+        // All child branches that are rooted after the current frame need to be deleted.
+        for (size_t i = 0; i < this->children.count; )
+        {
+            auto child = this->children[i];
+            assert(child != nullptr);
+            {
+                if (child->GetRootFrameIndex() > this->currentFrameIndex)
+                {
+                    this->_DeleteBranchByIndex(i);
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+        }
+
+        // Now the frames coming after the current one need to be deleted.
+        if (this->currentFrameIndex > this->rootFrameIndex)
+        {
+            while (this->frames.count > this->currentFrameIndex - this->rootFrameIndex)
+            {
+                auto frame = this->frames.GetBack();
+                assert(frame != nullptr);
+                {
+                    this->frames.PopBack();
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+    /////////////////////////////////////////////
+    // Internal Use Only
+
+    void SceneStateStackBranch::_DeleteBranchByIndex(size_t childIndex)
+    {
+        auto child = this->children[childIndex];
+        assert(child != nullptr);
+        {
+            this->children.Remove(childIndex);
+            delete child;
+        }
+    }
+
+
+
+
+
+    /////////////////////////////////////////////
+    // Private
+
+    SceneStateStackBranch* SceneStateStackBranch::FindBranch(uint32_t frameIndex)
+    {
+        if (frameIndex <= this->rootFrameIndex)
+        {
+            if (this->parent != nullptr)
+            {
+                return this->parent->FindBranch(frameIndex);
+            }
+        }
+
+        return this;
     }
 }
