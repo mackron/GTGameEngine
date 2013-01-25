fee9317d1854f74d30a2b177a9b391f1a7e572e2
Dave Reid <mackron@gmail.com>
2013-01-25 21:48:52 +1000

    Add a class representing the staging area of the scene state stack.
    This class isn't actually used anywhere yet.

---
+++ include/GTEngine/SceneStateStackStagingArea.hpp
@@ -0,0 +1,88 @@
+
+#ifndef __GTEngine_SceneStateStackStagingArea_hpp_
+#define __GTEngine_SceneStateStackStagingArea_hpp_
+
+#include <GTCore/Vector.hpp>
+#include <GTCore/Map.hpp>
+#include <GTCore/Serializer.hpp>
+#include <GTCore/Deserializer.hpp>
+
+namespace GTEngine
+{
+    class Scene;
+    class SceneStateStackBranch;
+
+    /// Class representing the staging area.
+    class SceneStateStackStagingArea
+    {
+    public:
+
+        /// Constructor.
+        SceneStateStackStagingArea(SceneStateStackBranch &branch);
+
+        /// Destructor.
+        ~SceneStateStackStagingArea();
+
+
+        /// Retrieves a reference to the branch that owns this staging area.
+              SceneStateStackBranch & GetBranch()       { return this->branch; }
+        const SceneStateStackBranch & GetBranch() const { return this->branch; }
+
+        /// Retrieves a reference to the relevant scene.
+              Scene & GetScene();
+        const Scene & GetScene() const;
+
+
+
+        /// Stages an insert command of the given scene node.
+        ///
+        /// @param sceneNodeID [in] The ID of the scene node that was inserted.
+        void StageInsert(uint64_t sceneNodeID);
+
+        /// Stages a delete command of the given scene node.
+        ///
+        /// @param sceneNodeID [in] The ID of the scene node that was deleted.
+        void StageDelete(uint64_t sceneNodeID);
+
+        /// Stages an update command of the given scene node.
+        ///
+        /// @param sceneNodeID [in] The ID of the scene node that was updated.
+        void StageUpdate(uint64_t sceneNodeID);
+
+
+        /// Clears the staging area.
+        void Clear();
+
+
+        /// Retrieves a reference to the insert commands.
+              GTCore::Vector<uint64_t> & GetInserts()       { return this->inserts; }
+        const GTCore::Vector<uint64_t> & GetInserts() const { return this->inserts; }
+
+        /// Retrieves a reference to the delete commands.
+              GTCore::Map<uint64_t, GTCore::BasicSerializer*> & GetDeletes()       { return this->deletes; }
+        const GTCore::Map<uint64_t, GTCore::BasicSerializer*> & GetDeletes() const { return this->deletes; }
+
+        /// Retrieves a reference to the update commands.
+              GTCore::Vector<uint64_t> & GetUpdates()       { return this->updates; }
+        const GTCore::Vector<uint64_t> & GetUpdates() const { return this->updates; }
+
+
+    private:
+
+        /// The branch that owns this staging area.
+        SceneStateStackBranch &branch;
+
+
+        /// The list of scene node ID's of newly inserted scene nodes in the staging area.
+        GTCore::Vector<uint64_t> inserts;
+
+        /// The list of scene node ID's of newly deleted scene nodes in the staging area. We need to keep track of the serialized data
+        /// because the scene will want to delete the node, after which point we won't be able to retrieve the data.
+        GTCore::Map<uint64_t, GTCore::BasicSerializer*> deletes;
+
+        /// The list of scene node ID's of newly updated scene nodes in the staging area.
+        GTCore::Vector<uint64_t> updates;
+    };
+}
+
+#endif
---
+++ source/SceneStateStackStagingArea.cpp
@@ -0,0 +1,102 @@
+
+#include <GTEngine/SceneStateStackStagingArea.hpp>
+#include <GTEngine/SceneStateStackBranch.hpp>
+#include <GTEngine/Scene.hpp>
+
+namespace GTEngine
+{
+    SceneStateStackStagingArea::SceneStateStackStagingArea(SceneStateStackBranch &branchIn)
+        : branch(branchIn), inserts(), deletes(), updates()
+    {
+    }
+
+    SceneStateStackStagingArea::~SceneStateStackStagingArea()
+    {
+    }
+
+
+    Scene & SceneStateStackStagingArea::GetScene()
+    {
+        return this->branch.GetScene();
+    }
+
+    const Scene & SceneStateStackStagingArea::GetScene() const
+    {
+        return this->branch.GetScene();
+    }
+
+
+    void SceneStateStackStagingArea::StageInsert(uint64_t sceneNodeID)
+    {
+        // If a delete command with the scene node is already staged, all we want to do is remove it from the deletes and just
+        // ignore everything.
+        auto iDelete = this->deletes.Find(sceneNodeID);
+        if (iDelete != nullptr)
+        {
+            delete iDelete->value;
+            this->deletes.RemoveByIndex(iDelete->index);
+        }
+        else
+        {
+            // If the scene node is in the updates list we need to remove it.
+            this->updates.RemoveFirstOccuranceOf(sceneNodeID);
+
+            if (!this->inserts.Exists(sceneNodeID))
+            {
+                this->inserts.PushBack(sceneNodeID);
+            }
+        }
+    }
+
+    void SceneStateStackStagingArea::StageDelete(uint64_t sceneNodeID)
+    {
+        // If an insert command with the scene node is already staged, all we want to do is remove it from the inserts and just
+        // ignore everything.
+        size_t index;
+        if (this->inserts.FindFirstIndexOf(sceneNodeID, index))
+        {
+            this->inserts.Remove(index);
+        }
+        else
+        {
+            // If the scene node is in the updates list we need to remove it.
+            this->updates.RemoveFirstOccuranceOf(sceneNodeID);
+
+            if (!this->deletes.Exists(sceneNodeID))
+            {
+                auto sceneNode = this->GetScene().GetSceneNodeByID(sceneNodeID);
+                assert(sceneNode != nullptr);
+                {
+                    auto sceneNodeSerializer = new GTCore::BasicSerializer;
+                    sceneNode->Serialize(*sceneNodeSerializer);
+
+                    this->deletes.Add(sceneNodeID, sceneNodeSerializer);
+                }
+            }
+        }
+    }
+
+    void SceneStateStackStagingArea::StageUpdate(uint64_t sceneNodeID)
+    {
+        // We ignore update commands if an insert or delete command is already present.
+        if (!this->inserts.Exists(sceneNodeID) &&
+            !this->deletes.Exists(sceneNodeID) &&
+            !this->updates.Exists(sceneNodeID))
+        {
+            this->updates.PushBack(sceneNodeID);
+        }
+    }
+
+
+    void SceneStateStackStagingArea::Clear()
+    {
+        for (size_t i = 0; i < this->deletes.count; ++i)
+        {
+            delete this->deletes.buffer[i]->value;
+        }
+
+        this->inserts.Clear();
+        this->deletes.Clear();
+        this->updates.Clear();
+    }
+}
