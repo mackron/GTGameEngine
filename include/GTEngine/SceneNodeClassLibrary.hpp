321baa0a4d2933c8b41d98c74e9dc73fd2476778
Dave Reid <mackron@gmail.com>
2013-02-06 15:00:56 +1000

    Add more groundwork for the prefab system.

--- include/GTEngine/SceneNodeClassLibrary.hpp
+++ include/GTEngine/SceneNodeClassLibrary.hpp
@@ -1,6 +1,9 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
 
 #ifndef __GTEngine_SceneNodeClassLibrary_hpp_
 #define __GTEngine_SceneNodeClassLibrary_hpp_
+
+#include "SceneNodeClass.hpp"
 
 namespace GTEngine
 {
@@ -9,17 +12,38 @@ namespace GTEngine
     {
     public:
 
-        /// Constructor.
-        SceneNodeClassLibrary();
+        //////////////////////////////////////
+        // Startup/Shutdown.
 
-        /// Destructor.
-        ~SceneNodeClassLibrary();
+        /// Starts up the library.
+        static bool Startup();
+
+        /// Shuts down the library.
+        static void Shutdown();
 
 
-
-    private:
         
+        //////////////////////////////////////
+        // Acquire/Unacquire
 
+        /// Acquires a scene node class by it's file name.
+        ///
+        /// @param fileName       [in] The file name of the class being loaded, relative to the data directory.
+        /// @param makeRelativeTo [in] If 'fileName' is absolute, this will be used to turn it into a relative path.
+        ///
+        /// @return A pointer to the class, or null if an error occurs.
+        ///
+        /// @remarks
+        ///     Each call to this function must match up to an Unacquire().
+        ///     @par
+        ///     All resources must have a relative path somewhere. If it doesn't, there will be errors with serialization. Thus,
+        ///     this will return null if 'fileName' is absolute and 'makeRelativeTo' is null.
+        static SceneNodeClass* Acquire(const char* fileName, const char* makeRelativeTo = nullptr);
+
+        /// Unacquires a class that was first acquired by Acquire().
+        ///
+        /// @param sceneNodeClassToUnacquire [in] A pointer to the scene node class to unacquire.
+        static void Unacquire(const SceneNodeClass* sceneNodeClassToUnacquire);
     };
 }
 
--- source/SceneNodeClassLibrary.cpp
+++ source/SceneNodeClassLibrary.cpp
@@ -1,16 +1,110 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
 
 #include <GTEngine/SceneNodeClassLibrary.hpp>
+#include <GTEngine/Errors.hpp>
+#include <GTCore/Path.hpp>
+#include <GTCore/Dictionary.hpp>
+#include <utility>
+
 
 namespace GTEngine
 {
-    SceneNodeClassLibrary::SceneNodeClassLibrary()
+    //////////////////////////////////////
+    // Globals.
+
+    typedef std::pair<SceneNodeClass*, size_t> SceneNodeClassReference;
+
+    /// The list of loaded classes, indexed by the absolute path.
+    static GTCore::Dictionary<SceneNodeClassReference> LoadedClasses;
+
+
+
+
+    //////////////////////////////////////
+    // Startup/Shutdown.
+
+    bool SceneNodeClassLibrary::Startup()
     {
+        return true;
     }
 
-    SceneNodeClassLibrary::~SceneNodeClassLibrary()
+    void SceneNodeClassLibrary::Shutdown()
     {
+        for (size_t i = 0; i < LoadedClasses.count; ++i)
+        {
+            delete LoadedClasses.buffer[i]->value.first;
+        }
+        LoadedClasses.Clear();
     }
 
 
 
+    //////////////////////////////////////
+    // Acquire/Unacquire
+
+    SceneNodeClass* SceneNodeClassLibrary::Acquire(const char* fileName, const char* makeRelativeTo)
+    {
+        GTCore::String relativePath(fileName);
+
+        if (GTCore::Path::IsAbsolute(fileName))
+        {
+            if (makeRelativeTo != nullptr)
+            {
+                relativePath = GTCore::IO::ToRelativePath(fileName, makeRelativeTo);
+            }
+            else
+            {
+                GTEngine::PostError("Attempting to load a file using an absolute path (%s). You need to use a path that's relative to the game's data directory.", fileName);
+                return nullptr;
+            }
+        }
+
+
+        GTCore::String absFileName;
+        if (GTCore::IO::FindAbsolutePath(fileName, absFileName))
+        {
+            auto iLoadedClass = LoadedClasses.Find(absFileName.c_str());
+            if (iLoadedClass == nullptr)
+            {
+                // Does not exist. Needs to be loaded.
+            }
+            else
+            {
+                // Already exists, so we just increment the counter.
+                ++iLoadedClass->value.second;
+
+                return iLoadedClass->value.first;
+            }
+        }
+
+
+        return nullptr;
+    }
+
+    void SceneNodeClassLibrary::Unacquire(const SceneNodeClass* sceneNodeClassToUnacquire)
+    {
+        if (sceneNodeClassToUnacquire != nullptr)
+        {
+            // We need to search by value.
+            for (size_t i = 0; i < LoadedClasses.count; ++i)
+            {
+                auto &value = LoadedClasses.buffer[i]->value;
+                if (value.first == sceneNodeClassToUnacquire)
+                {
+                    assert(value.second > 0);
+                    {
+                        --value.second;
+
+                        if (value.second == 0)
+                        {
+                            delete value.first;
+                            LoadedClasses.RemoveByIndex(i);
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
