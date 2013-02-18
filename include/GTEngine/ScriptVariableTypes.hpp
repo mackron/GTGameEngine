eac46eea09ac02a8a993f4f3f36c3e77ab144a45
Dave Reid <mackron@gmail.com>
2013-02-19 09:30:04 +1000

    Add skeleton ScriptVariable class.

---
+++ include/GTEngine/ScriptVariableTypes.hpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
+
+#ifndef __GTEngine_ScriptVariableTypes_hpp_
+#define __GTEngine_ScriptVariableTypes_hpp_
+
+namespace GTEngine
+{
+    enum ScriptVariableType
+    {
+        ScriptVariableType_Float,
+        ScriptVariableType_Integer,
+        ScriptVariableType_Float2,
+        ScriptVariableType_Float3,
+        ScriptVariableType_Float4,
+        ScriptVariableType_String,
+        ScriptVariableType_Prefab,
+    };
+}
+
+#endif
