016093fe9ad0688dbe8bf9d754ffd7e76d83a3c5
Dave Reid <mackron@gmail.com>
2013-02-18 11:29:36 +1000

    Add shell ScriptLibrary class.
    This is a static class that is responsible for loading script files.

---
+++ source/ScriptLibrary.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
+
+#include <GTEngine/ScriptLibrary.hpp>
+
+namespace GTEngine
+{
+    bool ScriptLibrary::Startup()
+    {
+        return true;
+    }
+
+    void ScriptLibrary::Shutdown()
+    {
+    }
+}
