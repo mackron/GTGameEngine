7eb5504523b0b00daa998c9e3767fe820f279475
Dave Reid <mackron@gmail.com>
2013-02-19 18:16:01 +1000

    Add classes for the remaining script variable types.

--- source/ScriptVariable.cpp
+++ source/ScriptVariable.cpp
@@ -68,6 +68,96 @@ namespace GTEngine
     }
 
 
+    /////////////////////////////////////
+    // Vec3
+
+    ScriptVariable_Vec3::ScriptVariable_Vec3(const char* name)
+        : ScriptVariable(ScriptVariableType_Vec3, name), x(0.0), y(0.0), z(0.0)
+    {
+    }
+
+    ScriptVariable_Vec3::ScriptVariable_Vec3(const ScriptVariable_Vec3 &other)
+        : ScriptVariable(other), x(other.GetX()), y(other.GetY()), z(other.GetZ())
+    {
+    }
+
+    ScriptVariable_Vec3::~ScriptVariable_Vec3()
+    {
+    }
+
+
+    /////////////////////////////////////
+    // Vec4
+
+    ScriptVariable_Vec4::ScriptVariable_Vec4(const char* name)
+        : ScriptVariable(ScriptVariableType_Vec4, name), x(0.0), y(0.0), z(0.0)
+    {
+    }
+
+    ScriptVariable_Vec4::ScriptVariable_Vec4(const ScriptVariable_Vec4 &other)
+        : ScriptVariable(other), x(other.GetX()), y(other.GetY()), z(other.GetZ()), w(other.GetW())
+    {
+    }
+
+    ScriptVariable_Vec4::~ScriptVariable_Vec4()
+    {
+    }
+
+
+    /////////////////////////////////////
+    // Boolean
+
+    ScriptVariable_Boolean::ScriptVariable_Boolean(const char* name)
+        : ScriptVariable(ScriptVariableType_Boolean, name), value(false)
+    {
+    }
+
+    ScriptVariable_Boolean::ScriptVariable_Boolean(const ScriptVariable_Boolean &other)
+        : ScriptVariable(other), value(other.GetValue())
+    {
+    }
+
+    ScriptVariable_Boolean::~ScriptVariable_Boolean()
+    {
+    }
+
+
+    /////////////////////////////////////
+    // String
+
+    ScriptVariable_String::ScriptVariable_String(const char* name)
+        : ScriptVariable(ScriptVariableType_String, name), value()
+    {
+    }
+
+    ScriptVariable_String::ScriptVariable_String(const ScriptVariable_String &other)
+        : ScriptVariable(other), value(other.GetValue())
+    {
+    }
+
+    ScriptVariable_String::~ScriptVariable_String()
+    {
+    }
+
+
+    /////////////////////////////////////
+    // Prefab
+
+    ScriptVariable_Prefab::ScriptVariable_Prefab(const char* name)
+        : ScriptVariable(ScriptVariableType_Prefab, name), value()
+    {
+    }
+
+    ScriptVariable_Prefab::ScriptVariable_Prefab(const ScriptVariable_Prefab &other)
+        : ScriptVariable(other), value(other.GetValue())
+    {
+    }
+
+    ScriptVariable_Prefab::~ScriptVariable_Prefab()
+    {
+    }
+
+
 
 
 
