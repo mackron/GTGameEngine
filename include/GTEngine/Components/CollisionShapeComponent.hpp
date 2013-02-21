4adadbb7932118a046f4dccd545add751c4821c7
Dave Reid <mackron@gmail.com>
2013-02-22 08:16:24 +1000

    Add skeleton base class for components needing collision shapes.
    This will be used by the dynamics and proximity components. The idea is to make it much easier to work with complex collision shapes without having to rewrite a bunch of code.

---
+++ include/GTEngine/Components/CollisionShapeComponent.hpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
+
+#ifndef __GTEngine_CollisionShapeComponent_hpp_
+#define __GTEngine_CollisionShapeComponent_hpp_
+
+#include "../Component.hpp"
+
+namespace GTEngine
+{
+    // Base class for components needing a collision shape.
+    //
+    // Classes inheritting from this class will need to setup the standard component properties manually.
+    //
+    // This class will call virtual methods when a certain action needs to take place such as removing and adding the object from the world.
+    class CollisionShapeComponent : public GTEngine::Component
+    {
+    public:
+
+        /// Constructor.
+        CollisionShapeComponent(SceneNode &node);
+
+        /// Destructor.
+        virtual ~CollisionShapeComponent();
+
+
+
+
+
+
+    private:
+    };
+}
+
+#endif
---
+++ source/Components/CollisionShapeComponent.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
+
+#include <GTEngine/Components/CollisionShapeComponent.hpp>
+
+namespace GTEngine
+{
+    CollisionShapeComponent::CollisionShapeComponent(SceneNode &node)
+        : Component(node)
+    {
+    }
+
+    CollisionShapeComponent::~CollisionShapeComponent()
+    {
+    }
+}
