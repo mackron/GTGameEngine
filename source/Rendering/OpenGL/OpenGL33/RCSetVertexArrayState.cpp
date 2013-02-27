5fcb3daf0078e03fbc8d49a42ded4240f3c94684
Dave Reid <mackron@gmail.com>
2013-02-27 11:12:12 +1000

    Some vertex array fixes.

--- source/Rendering/OpenGL/OpenGL33/RCSetVertexArrayState.cpp
+++ source/Rendering/OpenGL/OpenGL33/RCSetVertexArrayState.cpp
@@ -21,7 +21,7 @@ namespace GTEngine
     {
         assert(this->vertexArrayObject != vertexArrayObjectIn || this->vertexArrayObject == nullptr);
         {
-            delete this->vertexData.vertices;
+            free(this->vertexData.vertices);
 
             this->vertexData.vertices = static_cast<float*>(malloc(vertexCount * vertexSizeInBytes));
             memcpy(this->vertexData.vertices, vertices, vertexCount * vertexSizeInBytes);
@@ -40,7 +40,7 @@ namespace GTEngine
     {
         assert(this->vertexArrayObject != vertexArrayObjectIn || this->vertexArrayObject == nullptr);
         {
-            delete this->indexData.indices;
+            free(this->indexData.indices);
 
             this->indexData.indices = static_cast<unsigned int*>(malloc(indexCount * sizeof(unsigned int)));
             memcpy(this->indexData.indices, indices, indexCount * sizeof(unsigned int));
@@ -63,11 +63,13 @@ namespace GTEngine
             if ((this->operationBitfield & VERTEX_DATA_BIT))
             {
                 glBufferData(GL_ARRAY_BUFFER, this->vertexData.count * this->vertexData.vertexSizeInBytes, this->vertexData.vertices, this->vertexData.usage);
+                free(this->vertexData.vertices);
             }
 
             if ((this->operationBitfield & INDEX_DATA_BIT))
             {
                 glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indexData.count * sizeof(unsigned int), this->indexData.indices, this->indexData.usage);
+                free(this->indexData.indices);
             }
         }
     }
