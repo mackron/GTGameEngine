1cea63851079a1c595ba281b9d81c0f7ba8d40e3
Dave Reid <mackron@gmail.com>
2013-03-04 19:50:53 +1000

    Update the Texture2D library to use the new creation and deletion system.

--- include/GTEngine/Texture2DLibrary.hpp
+++ include/GTEngine/Texture2DLibrary.hpp
@@ -14,9 +14,10 @@ namespace GTEngine
     /// threads.
     class Texture2DLibrary
     {
-
-    // Startup/Shutdown
     public:
+
+        /////////////////////////////////////////////////////
+        // Startup/Shutdown
 
         /// Starts up the library.
         static bool Startup();
@@ -25,8 +26,9 @@ namespace GTEngine
         static void Shutdown();
 
 
-    // Acquiring/Unacquiring
-    public:
+
+        /////////////////////////////////////////////////////
+        // Acquiring/Unacquiring
 
         /// Acquires a texture object by it's name, relative to the data directory. See remarks.
         ///
@@ -56,7 +58,7 @@ namespace GTEngine
         /// Unacquires a texture.
         ///
         /// @param texture [in] A pointer to the texture to unacquire.
-        static void Unacquire(const Texture2D* texture);
+        static void Unacquire(Texture2D* texture);
 
 
         /// Reloads the texture of the given file.
@@ -71,15 +73,16 @@ namespace GTEngine
 
 
 
-    /// System/Engine textures.
-    public:
+        /////////////////////////////////////////////////////
+        // System/Engine textures.
 
         /// Retrieves a 1x1 black texture.
         static Texture2D* Get1x1BlackTexture();
 
 
-    /// Misc.
-    public:
+
+        /////////////////////////////////////////////////////
+        // Misc.
 
         /// Sets the default level of anisotropy to apply to newly acquired textures.
         static void SetDefaultAnisotropy(unsigned int defaultAnisotropy);
@@ -89,7 +92,6 @@ namespace GTEngine
 
         /// Sets the default magnification filter.
         static void SetDefaultMagFilter(TextureFilter filter);
-
 
         /// Determines whether or not the given extension is a supported image extension.
         static bool IsExtensionSupported(const char* extension);
