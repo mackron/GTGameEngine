685f82b56e7b531d9d935703e936de5929981be3
Dave Reid <mackron@gmail.com>
2013-03-04 23:39:16 +1000

    Separate some enumerators into their own files.

---
+++ include/GTEngine/Rendering/TextureWrapModes.hpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
+
+#ifndef __GTEngine_Renderering_TextureWrapModes_hpp_
+#define __GTEngine_Renderering_TextureWrapModes_hpp_
+
+namespace GTEngine
+{
+    enum TextureWrapMode
+    {
+        TextureWrapMode_Repeat,
+        TextureWrapMode_Clamp,
+        TextureWrapMode_ClampToEdge,
+    };
+}
+
+#endif
