527ef4510ed97b54c5ab6dacb987d4f27d0a08a4
Dave Reid <mackron@gmail.com>
2013-03-26 09:45:29 +1000

    More work on the material editor.

---
+++ bin/data/engine/editor/scripts/materialeditor.lua
@@ -0,0 +1,67 @@
+-- Copyright (C) 2011 - 2013 David Reid. See included LICENCE file.
+
+function GTGUI.Element:MaterialEditor(_internalPtr)
+    self._internalPtr    = _internalPtr;
+    self.IsLMBDown       = false;
+    self.IsRMBDown       = false;
+    self.HasMouseCapture = false;
+    
+    ----------------------------------------------
+    -- Preview  --          Properties          --
+    ----------------------------------------------
+    --                Code                      --
+    ----------------------------------------------
+    self.TopContainer      = GTGUI.Server.New("<div parentid='" .. self:GetID()                  .. "' styleclass='material-editor-top-container'      />");
+    self.BottomContainer   = GTGUI.Server.New("<div parentid='" .. self:GetID()                  .. "' styleclass='material-editor-bottom-container'   />");
+    self.TopLeftContainer  = GTGUI.Server.New("<div parentid='" .. self.TopContainer:GetID()     .. "' styleclass='material-editor-topleft-container'  />");
+    self.TopRightContainer = GTGUI.Server.New("<div parentid='" .. self.TopContainer:GetID()     .. "' styleclass='material-editor-topright-container' />");
+    self.Viewport          = GTGUI.Server.New("<div parentid='" .. self.TopLeftContainer:GetID() .. "' styleclass='material-editor-viewport'           />");
+    
+    
+    
+    self.Viewport:OnLMBDown(function()
+        self.IsLMBDown       = true;
+        self.HasMouseCapture = true;
+        self:Focus();
+    end);
+    
+    self.Viewport:OnRMBDown(function()
+        self.IsRMBDown       = true;
+        self.HasMouseCapture = true;
+        self:Focus();
+    end);
+
+    
+    
+    self:WatchLMBDown(function(data)
+        if self.HasMouseCapture then
+            self.IsLMBDown = true;
+        end
+    end);
+    
+    self:WatchRMBDown(function(data)
+        if self.HasMouseCapture then
+            self.IsRMBDown = true;
+        end
+    end);
+    
+    self:WatchLMBUp(function(data)
+        if not GTGUI.Server.IsRMBDown() then
+            Game.ReleaseMouse();
+            self.HasMouseCapture = false;
+        end
+        
+        self.IsLMBDown = false;
+    end);
+    
+    self:WatchRMBUp(function(data)
+        if not GTGUI.Server.IsLMBDown() then
+            Game.ReleaseMouse();
+            self.HasMouseCapture = false;
+        end
+        
+        self.IsRMBDown = false;
+    end);
+    
+    return self;
+end
--- include/GTEngine/Editor/MaterialEditor/MaterialEditor.hpp
+++ include/GTEngine/Editor/MaterialEditor/MaterialEditor.hpp
@@ -4,6 +4,8 @@
 #define __GTEngine_MaterialEditor_hpp_
 
 #include "../SubEditor.hpp"
+#include "../Editor3DViewportEventHandler.hpp"
+#include "../../Scene.hpp"
 
 namespace GTEngine
 {
@@ -19,9 +21,69 @@ namespace GTEngine
         ~MaterialEditor();
 
 
+        /// Resets the camera.
+        void ResetCamera();
+
+
+
+        ///////////////////////////////////////////////////
+        // Virtual Methods.
+
+        /// SubEditor::Show()
+        void Show();
+
+        /// SubEditor::Hide()
+        void Hide();
+
+        /// SubEditor::Save()
+        bool Save();
+
+        /// SubEditor::Update()
+        void OnUpdate(double deltaTimeInSeconds);
+
+
+        /// SubEditor::OnFileUpdate()
+        void OnFileUpdate(const DataFilesWatcher::Item &item);
+
+
 
     private:
 
+        /// Applies the camera rotation to the camera node.
+        void ApplyCameraRotation();
+
+
+    private:
+
+        /// The scene for the preview window.
+        Scene scene;
+
+        /// The viewport that the preview scene will be drawn from.
+        SceneViewport viewport;
+
+        /// The scene node acting as the camera for the preview window viewport.
+        SceneNode camera;
+
+        /// The scene node containing the model to use in the preview window.
+        SceneNode modelNode;
+
+
+        /// The main container element.
+        GTGUI::Element* mainElement;
+
+        /// The viewport element.
+        GTGUI::Element* viewportElement;
+
+        /// The viewport event handler.
+        Editor3DViewportEventHandler viewportEventHandler;
+
+
+        float cameraXRotation;      ///< The camera's current X rotation.
+        float cameraYRotation;      ///< The camera's current Y rotation.
+
+
+        /// The material to use with the model.
+        Material* material;
     };
 }
 
