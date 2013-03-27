ffb03440d5ffe6e707947db7be8575fd4bc63fbb
Dave Reid <mackron@gmail.com>
2013-03-28 09:16:04 +1000

    More work on the particle editor.

--- include/GTEngine/Editor/ParticleEditor/ParticleEditor.hpp
+++ include/GTEngine/Editor/ParticleEditor/ParticleEditor.hpp
@@ -1,5 +1,8 @@
 // Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
 
+#ifndef __GTEngine_ParticleEditor_hpp_
+#define __GTEngine_ParticleEditor_hpp_
+
 #include "../SubEditor.hpp"
 #include "../Editor3DViewportEventHandler.hpp"
 #include "../../Scene.hpp"
@@ -62,5 +65,34 @@ namespace GTEngine
 
         /// The scene node acting as the camera for the preview window viewport.
         SceneNode camera;
+
+        /// The scene node containing the particle system.
+        SceneNode particleNode;
+
+
+        /// The main container element.
+        GTGUI::Element* mainElement;
+
+
+        /// The viewport element.
+        GTGUI::Element* viewportElement;
+
+        /// The viewport event handler.
+        Editor3DViewportEventHandler viewportEventHandler;
+
+
+        float cameraXRotation;      ///< The camera's current X rotation.
+        float cameraYRotation;      ///< The camera's current Y rotation.
+
+
+        /// Keeps track of whether or not the editor is in the middle of saving. We use this in determining whether or not the settings should be
+        /// set when it detects a modification to the file on disk.
+        bool isSaving;
+
+        /// Keeps track of whether or not we are handling a reload. We use this in keeping track of whether or not to mark the file as modified
+        /// when the settings are changed.
+        bool isReloading;
     };
 }
+
+#endif
--- source/Editor/ParticleEditor/ParticleEditor.cpp
+++ source/Editor/ParticleEditor/ParticleEditor.cpp
@@ -1,21 +1,109 @@
 // Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
 
 #include <GTEngine/Editor/ParticleEditor/ParticleEditor.hpp>
+#include <GTEngine/Editor.hpp>
+#include <GTEngine/Game.hpp>
 
 namespace GTEngine
 {
-    ParticleEditor::ParticleEditor(Editor &editor, const char* absolutePath, const char* relativePath)
-        : SubEditor(editor, absolutePath, relativePath)
+    ParticleEditor::ParticleEditor(Editor &ownerEditor, const char* absolutePath, const char* relativePath)
+        : SubEditor(ownerEditor, absolutePath, relativePath),
+          scene(), viewport(), camera(), particleNode(),
+          mainElement(nullptr), viewportElement(nullptr), viewportEventHandler(ownerEditor.GetGame(), this->viewport),
+          cameraXRotation(0.0f), cameraYRotation(0.0f),
+          isSaving(false), isReloading(false)
     {
+        // We use the camera for our lights.
+        this->camera.AddComponent<GTEngine::CameraComponent>();
+        this->camera.AddComponent<GTEngine::DirectionalLightComponent>()->SetColour(0.4f, 0.4f, 0.4f);
+        this->camera.AddComponent<GTEngine::AmbientLightComponent>()->SetColour(0.25f, 0.25f, 0.25f);
+
+        // Viewport and Renderer.
+        this->viewport.SetCameraNode(this->camera);
+        this->scene.AddViewport(this->viewport);
+        this->scene.GetRenderer().EnableBackgroundColourClearing(0.5f, 0.5f, 0.5f);
+        
+
+        // HDR should be disabled for this.
+        static_cast<DefaultSceneRenderer &>(this->scene.GetRenderer()).DisableHDR();
+
+
+        // Now we setup the particle node.
+        this->particleNode.AddComponent<ParticleSystemComponent>();
+
+
+
+        auto &gui    = this->GetGUI();
+        auto &script = this->GetScript();
+
+        this->mainElement = gui.CreateElement("<div parentid='Editor_SubEditorContainer' styleclass='particle-editor' />");
+        assert(this->mainElement != nullptr);
+        {
+            // The main element is the ModelEditor element. We need to pass 'this' as the '_internalPtr' argument.
+            script.Get(GTCore::String::CreateFormatted("GTGUI.Server.GetElementByID('%s')", this->mainElement->id).c_str());
+            assert(script.IsTable(-1));
+            {
+                script.Push("ParticleEditor");
+                script.GetTableValue(-2);
+                assert(script.IsFunction(-1));
+                {
+                    script.PushValue(-2);       // 'self'
+                    script.Push(this);          // '_internalPtr'
+                    script.Call(2, 0);
+
+                    // At this point we will have the scene editor initialized in the scripting environment, and is sitting at the top of the stack. So now we
+                    // need to retrieve pointers to some of it's objects.
+                    script.Push("Viewport");
+                    script.GetTableValue(-2);
+                    assert(script.IsTable(-1));
+                    {
+                        script.Push("GetID");
+                        script.GetTableValue(-2);
+                        assert(script.IsFunction(-1));
+                        {
+                            script.PushValue(-2);   // <-- 'self'
+                            script.Call(1, 1);
+                            assert(script.IsString(-1));
+                            {
+                                this->viewportElement = gui.GetElementByID(script.ToString(-1));
+
+                                this->viewportElement->AttachEventHandler(this->viewportEventHandler);
+                                this->viewportElement->OnSize();
+                            }
+                            script.Pop(1);          // <-- return value from GetID()
+                        }
+                    }
+                    script.Pop(1);
+                }
+            }
+            script.Pop(1);
+        }
+
+
+        // Here we'll add the appropriate scene nodes to the preview scene.
+        this->scene.AddSceneNode(this->camera);
+        this->scene.AddSceneNode(this->particleNode);
+        
+        // Now position the camera.
+        this->ResetCamera();
     }
 
     ParticleEditor::~ParticleEditor()
     {
+        // GUI elements.
+        this->GetGUI().DeleteElement(this->mainElement);
     }
 
 
     void ParticleEditor::ResetCamera()
     {
+        this->camera.SetPosition(3.5f, 2.0f, 3.5f);
+        this->camera.LookAt(0.0f, 0.0f, 0.0f);
+
+        glm::vec3 cameraRotation = glm::eulerAngles(this->camera.GetWorldOrientation());
+        this->cameraXRotation = cameraRotation.x;
+        this->cameraYRotation = cameraRotation.y;
+        this->ApplyCameraRotation();
     }
 
 
@@ -24,10 +112,12 @@ namespace GTEngine
 
     void ParticleEditor::Show()
     {
+        this->mainElement->Show();
     }
 
     void ParticleEditor::Hide()
     {
+        this->mainElement->Hide();
     }
 
     bool ParticleEditor::Save()
@@ -37,6 +127,47 @@ namespace GTEngine
 
     void ParticleEditor::OnUpdate(double deltaTimeInSeconds)
     {
+        if (this->viewportElement->IsVisible())
+        {
+            auto &game = this->GetOwnerEditor().GetGame();       // <-- For ease of use.
+
+            // If the mouse is captured we may need to move the screen around.
+            if (game.IsMouseCaptured())
+            {
+                const float moveSpeed   = 0.05f;
+                const float rotateSpeed = 0.1f;
+
+                float mouseOffsetX;
+                float mouseOffsetY;
+                game.GetSmoothedMouseOffset(mouseOffsetX, mouseOffsetY);
+
+                if (game.IsMouseButtonDown(GTCore::MouseButton_Left))
+                {
+                    if (game.IsMouseButtonDown(GTCore::MouseButton_Right))
+                    {
+                        this->camera.MoveUp(  -mouseOffsetY * moveSpeed);
+                        this->camera.MoveRight(mouseOffsetX * moveSpeed);
+                    }
+                    else
+                    {
+                        this->camera.MoveForward(-mouseOffsetY * moveSpeed);
+                        this->cameraYRotation += -mouseOffsetX * rotateSpeed;
+                    }
+                }
+                else
+                {
+                    if (game.IsMouseButtonDown(GTCore::MouseButton_Right))
+                    {
+                        this->cameraXRotation += -mouseOffsetY * rotateSpeed;
+                        this->cameraYRotation += -mouseOffsetX * rotateSpeed;
+                    }
+                }
+
+                this->ApplyCameraRotation();
+            }
+
+            this->scene.Update(deltaTimeInSeconds);
+        }
     }
 
     void ParticleEditor::OnFileUpdate(const DataFilesWatcher::Item &item)
@@ -51,5 +182,8 @@ namespace GTEngine
 
     void ParticleEditor::ApplyCameraRotation()
     {
+        this->camera.SetOrientation(glm::quat());
+        this->camera.RotateY(this->cameraYRotation);
+        this->camera.RotateX(this->cameraXRotation);
     }
 }
