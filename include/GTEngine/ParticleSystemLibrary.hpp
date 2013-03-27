8184ab8cd7c9ee0acaf700b2001091d2378f3031
Dave Reid <mackron@gmail.com>
2013-03-28 09:27:04 +1000

    Add a shell class for the particle system library.

---
+++ include/GTEngine/ParticleSystemLibrary.hpp
@@ -0,0 +1,23 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
+
+#ifndef __GTEngine_ParticleSystemLibrary_hpp_
+#define __GTEngine_ParticleSystemLibrary_hpp_
+
+namespace GTEngine
+{
+    /// Static class for managing loaded particle systems.
+    class ParticleSystemLibrary
+    {
+    public:
+
+        
+
+
+
+    private:
+
+
+    };
+}
+
+#endif
