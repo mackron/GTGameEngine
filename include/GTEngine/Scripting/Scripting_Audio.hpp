8b222a813e05f4eb602e31774f9ceb7d8c7d32d8
Dave Reid <mackron@gmail.com>
2013-04-05 08:46:22 +1000

    Add some improvements to the audio scripting library.

--- include/GTEngine/Scripting/Scripting_Audio.hpp
+++ include/GTEngine/Scripting/Scripting_Audio.hpp
@@ -23,7 +23,21 @@ namespace GTEngine
             ///
             /// @remarks
             ///     Argument 1: The file name of the sound to play.
+            ///     Argument 2: A math.vec3 representing the position of the sound. Can be nil, in which case it will be played in stereo mode (no positioning).
+            ///     Argument 3: Whether or not the sound should be positioned relative to the listener.
             int Play(GTCore::Script &script);
+
+            /// Sets the position of the listener.
+            ///
+            /// @remarks
+            ///     Argument 1: A math.vec3 representing the new position of the listener.
+            int SetListenerPosition(GTCore::Script &script);
+
+            /// Sets the orientation of the listener.
+            ///
+            /// @remarks
+            ///     Argument 1: A math.quat representing the new orientation of the listener.
+            int SetListenerOrientation(GTCore::Script &script);
         }
     }
 }
