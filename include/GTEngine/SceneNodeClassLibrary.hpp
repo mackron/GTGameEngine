6f1918ec27fcee903dcd3fa88026b56f872a8cae
Dave Reid <mackron@gmail.com>
2013-04-06 09:53:41 +1000

    Cleanup.

--- include/GTEngine/SceneNodeClassLibrary.hpp
+++ include/GTEngine/SceneNodeClassLibrary.hpp
@@ -45,32 +45,6 @@ namespace GTEngine
         ///
         /// @param sceneNodeClassToUnacquire [in] A pointer to the scene node class to unacquire.
         static void Unacquire(const SceneNodeClass* sceneNodeClassToUnacquire);
-
-
-#if 0
-        /// Creates a new scene node class, returning false if it already exists.
-        ///
-        /// @param absolutePath [in] The absolute path to save the new class to.
-        /// @param relativePath [in] The relative path to save the new class to.
-        /// @param sceneNode    [in] The scene node to create the class from.
-        ///
-        /// @remarks
-        ///     This does not acquire the class. Do that manually after creating.
-        static bool Create(const char* absolutePath, const char* relativePath, const SceneNode &sceneNode);
-
-        /// Updates a scene node class.
-        ///
-        /// @param absolutePath [in] The absolute path of the class to update.
-        /// @param sceneNodes   [in] The list of scene nodes making up the prefab.
-        /// @param hierarchy    [in] The hierarchy, containing indices to sceneNodes.
-        ///
-        /// @remarks
-        ///     This returns false if the file does not exist.
-        ///     @par
-        ///     The second item in the pairs in sceneNodes if the local ID to use with the prefab. When set to 0, a new ID will be generated. The very
-        ///     first ID must be 1, which is the root ID.
-        static bool Update(const char* absolutePath, const GTCore::Vector<std::pair<SceneNode*, uint64_t>> &sceneNodes, const GTCore::Map<size_t, size_t> &hierarchy);
-#endif
     };
 }
 
