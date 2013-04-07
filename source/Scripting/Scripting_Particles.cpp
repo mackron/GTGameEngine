3db9a19bf7032fa14d30dd6265c3398e8af0a92e
Dave Reid <mackron@gmail.com>
2013-04-08 08:55:31 +1000

    Add shell files for the particles scripting library.

---
+++ source/Scripting/Scripting_Particles.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
+
+#include <GTEngine/Scripting/Scripting_Particles.hpp>
+
+namespace GTEngine
+{
+    namespace Scripting
+    {
+        bool LoadParticlesLibrary(GTCore::Script &script)
+        {
+            (void)script;
+
+            return true;
+        }
+
+
+        namespace ParticleFFI
+        {
+        }
+
+        namespace ParticleEmitterFFI
+        {
+        }
+
+        namespace ParticleSystemFFI
+        {
+        }
+    }
+}
