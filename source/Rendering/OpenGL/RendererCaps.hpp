79096926a2b7ad9deca421fb355876e16cea1982
Dave Reid <mackron@gmail.com>
2013-04-12 18:13:12 +1000

    A few minor tweaks to the OpenGL 3.3 renderer.

--- source/Rendering/OpenGL/RendererCaps.hpp
+++ source/Rendering/OpenGL/RendererCaps.hpp
@@ -9,9 +9,14 @@ namespace GTEngine
 {
     struct RendererCaps_OpenGL
     {
-        GLint MaxColourAttachments;         ///< The maximum number of framebuffer colour attachments.
-        GLint MaxDrawBuffers;               ///< The maximum number of draw buffers for MRT.
-        GLint MaxTextureUnits;              ///< The maximum number of active texture units.
+        GLint MaxColourAttachments;             ///< The maximum number of framebuffer colour attachments.
+        GLint MaxDrawBuffers;                   ///< The maximum number of draw buffers for MRT.
+        GLint MaxTextureUnits;                  ///< The maximum number of active texture units.
+
+        GLint MaxVertexShaderInstructions;      ///< The maximum number of vertex shader instructions per shader.
+        GLint MaxFragmentShaderInstructions;    ///< The maximum number of fragment shader instructions per shader.
+        GLint MaxGeometryShaderInstructions;    ///< The maximum number of geometry shader instructions per shader.
+        GLint MaxShaderUniforms;                ///< The maximum number of uniforms per shader.
     };
 }
 
