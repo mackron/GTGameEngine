31e43d2d2a66f9571c3d8363dd947639ef6edc08
Dave Reid <mackron@gmail.com>
2013-04-29 09:36:17 +1000

    Introduce the notion of a shadow visibility processor.
    This class is responsible for processing and storing meshes that will be used as shadow casters for lights.

--- source/DefaultSceneRenderer/DefaultSceneRenderer.cpp
+++ source/DefaultSceneRenderer/DefaultSceneRenderer.cpp
@@ -12,49 +12,6 @@
 namespace GTEngine
 {
     ///////////////////////////////////////////////////////////
-    // DefaultSceneRendererShadowObjects
-
-    DefaultSceneRendererShadowObjects::DefaultSceneRendererShadowObjects()
-        : meshes()
-    {
-    }
-
-    DefaultSceneRendererShadowObjects::~DefaultSceneRendererShadowObjects()
-    {
-    }
-
-    void DefaultSceneRendererShadowObjects::ProcessModel(const SceneNode &sceneNode)
-    {
-        auto modelComponent = sceneNode.GetComponent<ModelComponent>();
-        assert(modelComponent != nullptr);
-        {
-            if (modelComponent->IsShadowCastingEnabled())       // <-- Ignore the model if it is not casting shadows.
-            {
-                auto model = modelComponent->GetModel();
-                if (model != nullptr)                           // <-- Is allowed to be null. Perhaps due to a bad path?
-                {
-                    for (size_t i = 0; i < model->meshes.count; ++i)
-                    {
-                        auto mesh = model->meshes[i];
-                        assert(mesh != nullptr);
-                        {
-                            DefaultSceneRendererMesh object;
-                            object.vertexArray = mesh->GetSkinnedGeometry();
-                            object.drawMode    = mesh->GetDrawMode();
-                            object.material    = mesh->GetMaterial();
-                            object.transform   = sceneNode.GetWorldTransform();
-                            this->meshes.PushBack(object);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-
-
-
-    ///////////////////////////////////////////////////////////
     // DefaultSceneRendererVisibleObjects
 
     DefaultSceneRendererVisibleObjects::DefaultSceneRendererVisibleObjects(Scene &sceneIn, SceneViewport &viewportIn)
