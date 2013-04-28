591c27a567b4faf59bc4c4c54e9047118b8bcba5
Dave Reid <mackron@gmail.com>
2013-04-28 17:32:59 +1000

    The first of many commits restructuring the code for the default scene renderer.

--- source/DefaultSceneRenderer/DefaultSceneRenderer.cpp
+++ source/DefaultSceneRenderer/DefaultSceneRenderer.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2011 - 2013 David Reid. See included LICENCE file or GTEngine.hpp.
 
-#include <GTEngine/DefaultSceneRenderer.hpp>
+#include <GTEngine/DefaultSceneRenderer/DefaultSceneRenderer.hpp>
 #include <GTEngine/Scene.hpp>
 #include <GTEngine/ShaderLibrary.hpp>
 
