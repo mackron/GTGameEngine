ce97deb2def1d2d1f0f6519b6c833281d5faf3d8
Dave Reid <mackron@gmail.com>
2013-05-03 08:03:49 +1000

    Some foundations for the new dynamic material shader system.

--- include/GTEngine/DefaultSceneRenderer/DefaultSceneRenderer_MaterialShaders.hpp
+++ include/GTEngine/DefaultSceneRenderer/DefaultSceneRenderer_MaterialShaders.hpp
@@ -4,9 +4,41 @@
 #define __GTEngine_DefaultSceneRenderer_MaterialShaders_hpp_
 
 #include "../Rendering/Shader.hpp"
+#include "DefaultSceneRenderer_LightGroup.hpp"
 
 namespace GTEngine
 {
+    /// Structure representing an ID of a material's shader.
+    ///
+    /// A material shader ID is created from a light group ID and material-specific flags.
+    struct DefaultSceneRenderer_MaterialShaderID
+    {
+        /// Constructor.
+        DefaultSceneRenderer_MaterialShaderID(const DefaultSceneRenderer_LightGroupID &lightGroupIDIn, uint32_t flagsIn)
+            : lightGroupID(lightGroupIDIn), flags(flagsIn)
+        {
+        }
+
+
+
+        /// Enumerator containing the different flags that can be used in a material shader ID.
+        enum Flags
+        {
+            IncludeMaterialPass = (1 << 1),
+            NoNormalMapping     = (1 << 2)
+        };
+
+
+        /// The light group ID.
+        DefaultSceneRenderer_LightGroupID lightGroupID;
+
+        /// The material-specific flags. Defaults to zero.
+        uint32_t flags;
+    };
+
+
+
+
     /// Structure containing the shaders associated with a material.
     struct DefaultSceneRenderer_MaterialShaders
     {
@@ -42,6 +74,11 @@ namespace GTEngine
 
         /// The shader to use when doing the material pass.
         Shader* materialShader;
+
+
+
+        /// The map of shaders, keyed by a material shader ID.
+        GTCore::Map<DefaultSceneRenderer_MaterialShaderID, Shader*> shaders;
 
 
     private:    // No copying.
