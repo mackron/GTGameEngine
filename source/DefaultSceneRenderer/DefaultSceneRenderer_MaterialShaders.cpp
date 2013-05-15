3cee6fcffe00aa59959f67eb73d871c718fcb310
Dave Reid <mackron@gmail.com>
2013-05-15 15:02:58 +1000

    More cleanup.

--- source/DefaultSceneRenderer/DefaultSceneRenderer_MaterialShaders.cpp
+++ source/DefaultSceneRenderer/DefaultSceneRenderer_MaterialShaders.cpp
@@ -6,26 +6,11 @@
 namespace GTEngine
 {
     DefaultSceneRenderer_MaterialShaders::DefaultSceneRenderer_MaterialShaders()
-        : ambientLightShader(nullptr),
-          directionalLightShader(nullptr),
-          pointLightShader(nullptr),
-          spotLightShader(nullptr),
-          shadowDirectionalLightShader(nullptr),
-          shadowPointLightShader(nullptr),
-          shadowSpotLightShader(nullptr),
-          materialShader(nullptr)
+        : shaders()
     {
     }
 
     DefaultSceneRenderer_MaterialShaders::~DefaultSceneRenderer_MaterialShaders()
     {
-        Renderer::DeleteShader(this->ambientLightShader);
-        Renderer::DeleteShader(this->directionalLightShader);
-        Renderer::DeleteShader(this->pointLightShader);
-        Renderer::DeleteShader(this->spotLightShader);
-        Renderer::DeleteShader(this->shadowDirectionalLightShader);
-        Renderer::DeleteShader(this->shadowPointLightShader);
-        Renderer::DeleteShader(this->shadowSpotLightShader);
-        Renderer::DeleteShader(this->materialShader);
     }
 }
