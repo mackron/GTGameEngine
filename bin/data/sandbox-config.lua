11c2631c6f0e13168dd7b6a3f02ab2ce403e8491
Dave Reid <mackron@gmail.com>
2013-06-03 12:52:34 +1000

    Add the initial version of the sandbox.

---
+++ bin/data/sandbox-config.lua
@@ -0,0 +1,17 @@
+-- This script will be executed when the sandbox is first run. You can execute your own scripts by calling
+-- GTEngine.ExecuteFile("my-script-file").
+--
+-- These scripts are executed on the main game script which means your game objects can access any properties
+-- declared here whenever they are needed.
+--
+-- This is executed as a standard Lua script. You can use dynamic logic here like branching and loops.
+
+--Game.StartupScene = "";
+Game.StartupScene = "sandbox/intro.gtscene";        -- The relative path of the initial scene to load. This is relative to the data directory. If this is not a valid scene, the editor will be opened.
+Game.Title        = "GTGameEngine Sandbox"          -- The title to show on the game window.
+Game.Width        = 1280;                           -- The horiziontal resolution.
+Game.Height       = 720;                            -- The vertical resolution.
+Game.Fullscreen   = false;                          -- Whether or not to start in fullscreen mode.
+Game.EnableVSync  = false;                          -- Whether or not to enable v-sync.
+Game.AspectRatio  = 0;                              -- If set to 0, the aspect ratio will be re-calculated whenever the game window is resized as the window width divided by height.
+--Game.AspectRatio  = Game.Width / Game.Height;       -- Use this to use a constant aspect ratio. Might also like to use "16 / 9", "4 / 3", etc (without quotes).
