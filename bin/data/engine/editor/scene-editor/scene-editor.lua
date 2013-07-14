a5595d2fbfef3535348859b14879481e29be3316
Dave Reid <mackron@gmail.com>
2013-07-15 08:26:21 +1000

    Small refactor to the Editor.
    This is part of a larger refactor.

--- bin/data/engine/editor/scene-editor/scene-editor.lua
+++ bin/data/engine/editor/scene-editor/scene-editor.lua
@@ -337,63 +337,6 @@ function Editor.SceneEditor.CreateComponentPanel(parentPanel, componentID)
 end
 
 
-function GTGUI.Element:CameraComponentPanel()
-    self:PanelGroupBox("Camera", true);
-    
-    -- FOV
-    self.FOVInput = GTGUI.Server.New("<div parentid='" .. self.Body:GetID() .. "' styleclass='labelled-number-input' />");
-    self.FOVInput:LabelledNumberInput("FOV");
-    self.FOVInput:UseFloatFormat();
-    
-    -- zNear
-    self.NearInput = GTGUI.Server.New("<div parentid='" .. self.Body:GetID() .. "' styleclass='labelled-number-input' />");
-    self.NearInput:LabelledNumberInput("Near Plane");
-    self.NearInput:UseFloatFormat();
-    
-    -- zFar
-    self.FarInput = GTGUI.Server.New("<div parentid='" .. self.Body:GetID() .. "' styleclass='labelled-number-input' />");
-    self.FarInput:LabelledNumberInput("Far Plane");
-    self.FarInput:UseFloatFormat();
-    
-    
-    
-    self.FOVInput:OnValueChanged(function(data)
-        self:Update3DProjection();
-        self.ParentPanel:OnSceneNodeChanged();
-    end);
-    
-    self.NearInput:OnValueChanged(function(data)
-        self:Update3DProjection();
-        self.ParentPanel:OnSceneNodeChanged();
-    end);
-    
-    self.FarInput:OnValueChanged(function(data)
-        self:Update3DProjection();
-        self.ParentPanel:OnSceneNodeChanged();
-    end);
-    
-    
-    
-    function self:Update(node)
-        self.CurrentNode      = node;
-        self.CurrentComponent = node:GetComponent(GTEngine.Components.Camera);
-        
-        self.FOVInput:SetValue(self.CurrentComponent:GetFOV());
-        self.NearInput:SetValue(self.CurrentComponent:GetNearClippingPlane());
-        self.FarInput:SetValue(self.CurrentComponent:GetFarClippingPlane());
-    end
-    
-    function self:Update3DProjection()
-        local fov    = self.FOVInput:GetValue();
-        local aspect = 16.0 / 9.0;
-        local znear  = self.NearInput:GetValue();
-        local zfar   = self.FarInput:GetValue();
-        self.CurrentComponent:Set3DProjection(fov, aspect, znear, zfar);
-    end
-    
-    return self;
-end
-
 
 function GTGUI.Element:ModelComponentPanel()
     self:PanelGroupBox("Model", true);
