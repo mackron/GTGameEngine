e2491bd03aa9c109ef33f08e6c83fd9c2f0b961c
David Reid <mackron@gmail.com>
2013-10-03 07:18:33 +1000

    More work on the GUI for collision groups.

--- bin/data/engine/editor/common/labelled-collision-group-selector.lua
+++ bin/data/engine/editor/common/labelled-collision-group-selector.lua
@@ -33,14 +33,39 @@ function GTGUI.Element:LabelledCollisionGroupSelector(title)
         self:AddSelectedGroup("None");
     end);
     
-
+    -- We now want to loop over each collision group in Game.CollisionGroup and add it to the list.
+    if Game.CollisionGroups then
+        for key,value in pairs(Game.CollisionGroups) do
+            if key ~= "All" and key ~= "None" then
+                self.DropDownBox:AppendItem(key):OnPressed(function()
+                    self:AddSelectedGroup(key);
+                end);
+            end
+        end
+    else
+        self.DropDownBox:AppendItem("No Groups");
+    end
 
 
     function self:RemoveAllSelectedGroups()
         self.SelectedItemsContainer:DeleteAllChildren();
     end
 
+    function self:RemoveSelectedGroupByName(name)
+        GTGUI.Server.DeleteElement(self:GetSelectedGroupByName(name));
+    end
+
     function self:AddSelectedGroup(name)
+        -- If we already have a selection of the same name, we just return the existing one instead of re-adding it.
+        local existingItem = self:GetSelectedGroupByName(name);
+        if existingItem then
+            return existingItem;
+        end
+
+        -- If we have an "All" or "None" group, that also needs to be deleted.
+        self:RemoveSelectedGroupByName("All");
+        self:RemoveSelectedGroupByName("None");
+
         local newItem = GTGUI.Server.CreateElement(self.SelectedItemsContainer, "labelled-collision-group-selector-item");
         newItem:LabelledCollisionGroupSelectorItem(name);
 
@@ -52,6 +77,15 @@ function GTGUI.Element:LabelledCollisionGroupSelector(title)
         return newItem;
     end
 
+    function self:GetSelectedGroupByName(name)
+        for id,element in pairs(self.SelectedItemsContainer.Children) do
+            if element.Label and element.Label:GetText() == name then
+                return element;
+            end
+        end
+
+        return nil;
+    end
 
     return self;
 end
