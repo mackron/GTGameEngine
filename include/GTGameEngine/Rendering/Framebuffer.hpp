51219180648dc1fd8d07de13ef8246f486503f75
David Reid <mackron@gmail.com>
2015-02-03 05:58:39 +1000

    Add some empty files in preparation for adding support for framebuffers and textures to the rendering system.

---
+++ include/GTGameEngine/Rendering/Framebuffer.hpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2011 - 2015 David Reid. See included LICENCE file.
+
+#ifndef __GT_GE_Framebuffer_hpp_
+#define __GT_GE_Framebuffer_hpp_
+
+namespace GT
+{
+    namespace GE
+    {
+        /// Class representing a framebuffer for the rendering system.
+        class Framebuffer
+        {
+        public:
+
+            /// Constructor.
+            Framebuffer();
+
+            /// Destructor.
+            virtual ~Framebuffer();
+
+
+
+
+
+
+        private:    // No copying.
+            Framebuffer(const Framebuffer &);
+            Framebuffer & operator=(const Framebuffer&);
+        };
+    }
+}
+
+#endif // !__GT_GE_Framebuffer_hpp_
