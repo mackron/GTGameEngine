8e71181df1a95048332b373cbffd6e59f4b5a199
David Reid <mackron@gmail.com>
2015-03-15 13:21:57 +1000

    Add some virtual methods to the ImageAsset class.
    These methods are used to retrieve relevant information about the image which
    can be used to create textures.

--- include/GTGameEngine/ImageAsset.hpp
+++ include/GTGameEngine/ImageAsset.hpp
@@ -4,6 +4,7 @@
 #define __GT_ImageAsset_hpp_
 
 #include "Asset.hpp"
+#include "Rendering/RenderingTypes.hpp"
 
 namespace GT
 {
@@ -21,6 +22,27 @@ namespace GT
 
         /// @copydoc Asset::GetClass()
         AssetClass GetClass() const;
+
+
+        /// Retrieves the width of the image.
+        ///
+        /// @return The width of the image.
+        virtual unsigned int GetImageWidth() const = 0;
+
+        /// Retrieves the height of the image.
+        ///
+        /// @return The height of the image.
+        virtual unsigned int GetImageHeight() const = 0;
+
+        /// Retrieves the format of the image.
+        ///
+        /// @return The format of the image.
+        virtual TextureFormat GetImageFormat() const = 0;
+
+        /// Retrieves a pointer to the data of the image.
+        ///
+        /// @return A pointer to the image data.
+        virtual void* GetImageData() const = 0;
     };
 }
 
