21d74628f8ea20c5096c9043ce59261b0c91164b
David Reid <mackron@gmail.com>
2015-05-17 19:06:04 +1000

    Initial work on material assets.

--- include/GTGameEngine/Assets/MaterialAsset.hpp
+++ include/GTGameEngine/Assets/MaterialAsset.hpp
@@ -7,6 +7,51 @@
 
 namespace GT
 {
+    /// Class representing a channel in a material.
+    ///
+    /// A channel is basically just a function that defines a particular property of a material. For example, a texture might be implemented using
+    /// the a channel called "diffuse".
+    class MaterialChannel
+    {
+    public:
+
+        /// Constructor.
+        MaterialChannel(const char* name, const char* shaderCode);
+
+        
+        /// Sets the name of the channel.
+        ///
+        /// @param name [in] The name of the channel. This cannot be mnore than 31 characters in length, and must be null terminated.
+        void SetName(const char* name);
+
+        /// Retrieves the name of the channel.
+        ///
+        /// @return The name of the channel.
+        const char* GetName() const;
+
+
+        /// Sets the shader code of the channel.
+        ///
+        /// @param shaderCode [in] The shader code.
+        void SetShaderCode(const char* shaderCode);
+
+        /// Retrieves the shader code of the channel.
+        ///
+        /// @return The shader code.
+        const char* GetShaderCode() const;
+
+
+    private:
+
+        /// The name of the channel.
+        char m_name[32];
+
+        /// The shader string containing the function that implements the channel. This is actually temporary, and will be changed to raw SPIR-V code.
+        GTLib::String m_shaderCode;
+    };
+
+
+
     /// Base class for material assets.
     class MaterialAsset : public Asset
     {
@@ -21,6 +66,40 @@ namespace GT
 
         /// @copydoc Asset::GetClass()
         AssetClass GetClass() const;
+
+
+
+        /// Adds a channel to the material.
+        ///
+        /// @param name       [in] The name of the channel. This cannot be more than 31 characters in length.
+        /// @param shaderCode [in] The shader code.
+        ///
+        /// @remarks
+        ///     If a channel of the same name already exists it will be removed.
+        void AddChannel(const char* name, const char* shaderCode);
+
+        /// Removes a channel by the channel name.
+        ///
+        /// @param name [in] The name of the channel to remove.
+        void RemoveChannel(const char* name);
+
+        /// Sets the shader code for the channel with the given name.
+        ///
+        /// @param name       [in] The name of the channel whose shader code is being set.
+        /// @param shaderCode [in] The shader code.
+        void SetChannelShaderCode(const char* name, const char* shaderCode);
+
+        /// Retrieves the shader code for the channel with the given name.
+        ///
+        /// @param name [in] The name of the channel whose shader code is being retrieved.
+        const char* GetChannelShaderCode(const char* name) const;
+
+
+
+    private:
+
+        /// The list of channels making up the material.
+        GTLib::Vector<MaterialChannel*> m_channels;
     };
 }
 
--- source/Assets/MaterialAsset.cpp
+++ source/Assets/MaterialAsset.cpp
@@ -2,15 +2,80 @@
 
 #include <GTGameEngine/Assets/MaterialAsset.hpp>
 
+#if defined(_MSC_VER)
+    #pragma warning(push)
+    #pragma warning(disable:4351)   // new behaviour
+#endif
+
 namespace GT
 {
+    /////////////////////////////////////
+    // MaterialChannel
+
+    MaterialChannel::MaterialChannel(const char* name, const char* shaderCode)
+        : m_name(), m_shaderCode(shaderCode)
+    {
+        // Name.
+        for (int i = 0; i < 4; ++i)
+        {
+            reinterpret_cast<uint64_t*>(m_name)[i] = 0;
+        }
+
+        for (int i = 0; i < 31 && name[i] != '\0'; ++i)
+        {
+            m_name[i] = name[i];
+        }
+    }
+
+
+    void MaterialChannel::SetName(const char* name)
+    {
+        // Name.
+        for (int i = 0; i < 4; ++i)
+        {
+            reinterpret_cast<uint64_t*>(m_name)[i] = 0;
+        }
+
+        for (int i = 0; i < 31 && name[i] != '\0'; ++i)
+        {
+            m_name[i] = name[i];
+        }
+    }
+
+    const char* MaterialChannel::GetName() const
+    {
+        return m_name;
+    }
+
+
+    void MaterialChannel::SetShaderCode(const char* shaderCode)
+    {
+        m_shaderCode = shaderCode;
+    }
+
+    const char* MaterialChannel::GetShaderCode() const
+    {
+        return m_shaderCode.c_str();
+    }
+
+
+
+
+    /////////////////////////////////////
+    // MaterialAsset
+
     MaterialAsset::MaterialAsset(AssetType assetType)
-        : Asset(assetType)
+        : Asset(assetType),
+          m_channels()
     {
     }
 
     MaterialAsset::~MaterialAsset()
     {
+        for (size_t iChannel = 0; iChannel < m_channels.GetCount(); ++iChannel)
+        {
+            delete m_channels[iChannel];
+        }
     }
 
 
@@ -18,4 +83,90 @@ namespace GT
     {
         return AssetClass_Material;
     }
+
+
+    void MaterialAsset::AddChannel(const char* name, const char* shaderCode)
+    {
+        // Create the new chunk.
+        MaterialChannel* pNewChannel = new MaterialChannel(name, shaderCode);
+
+
+        // Check if a chunk of the same name already exists.
+        for (size_t iChannel = 0; iChannel < m_channels.GetCount(); ++iChannel)
+        {
+            auto pChannel = m_channels[iChannel];
+            assert(pChannel != nullptr);
+            {
+                if (strcmp(name, pChannel->GetName()) == 0)
+                {
+                    // A chunk of the same name exists - replace it.
+                    delete pChannel;
+                    m_channels[iChannel] = pNewChannel;
+
+                    return;
+                }
+            }
+        }
+
+        // If we get here it means a chunk of the same name does not exist. In this case we just push it to the back.
+        m_channels.PushBack(pNewChannel);
+    }
+
+    void MaterialAsset::RemoveChannel(const char* name)
+    {
+        for (size_t iChannel = 0; iChannel < m_channels.GetCount(); ++iChannel)
+        {
+            auto pChannel = m_channels[iChannel];
+            assert(pChannel != nullptr);
+            {
+                if (strcmp(name, pChannel->GetName()) == 0)
+                {
+                    // We found the chunk.
+                    delete pChannel;
+                    m_channels.Remove(iChannel);
+
+                    return;
+                }
+            }
+        }
+    }
+
+    void MaterialAsset::SetChannelShaderCode(const char* name, const char* shaderCode)
+    {
+        for (size_t iChannel = 0; iChannel < m_channels.GetCount(); ++iChannel)
+        {
+            auto pChannel = m_channels[iChannel];
+            assert(pChannel != nullptr);
+            {
+                if (strcmp(name, pChannel->GetName()) == 0)
+                {
+                    // We found the chunk.
+                    pChannel->SetShaderCode(shaderCode);
+                }
+            }
+        }
+    }
+
+    const char* MaterialAsset::GetChannelShaderCode(const char* name) const
+    {
+        for (size_t iChannel = 0; iChannel < m_channels.GetCount(); ++iChannel)
+        {
+            auto pChannel = m_channels[iChannel];
+            assert(pChannel != nullptr);
+            {
+                if (strcmp(name, pChannel->GetName()) == 0)
+                {
+                    // We found the chunk.
+                    return pChannel->GetShaderCode();
+                }
+            }
+        }
+
+        // The chunk doesn't exist.
+        return nullptr;
+    }
 }
+
+#if defined(_MSC_VER)
+    #pragma warning(pop)
+#endif
