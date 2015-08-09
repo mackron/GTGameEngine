bc8224fcee2b5303745f4bc6569081f8be8ef926
David Reid <mackron@gmail.com>
2015-08-10 00:01:52 +1000

    Add support for retrieving input and property variables of a material asset.

--- include/GTGameEngine/Assets/MaterialAsset.hpp
+++ include/GTGameEngine/Assets/MaterialAsset.hpp
@@ -4,9 +4,75 @@
 #define __GT_MaterialAsset_hpp_
 
 #include "Asset.hpp"
+#include "../Graphics/GraphicsWorld.hpp"    // For material variable types.
 
 namespace GT
 {
+    struct MaterialVariable
+    {
+        /// The name of the variable. Cannot be more than 27 characters + null terminator.
+        char name[28];
+
+        /// The variable type.
+        GraphicsMaterialVariableType type;
+
+        /// The value.
+        union
+        {
+            struct
+            {
+                float x;
+            } f1;
+            struct
+            {
+                float x;
+                float y;
+            } f2;
+            struct
+            {
+                float x;
+                float y;
+                float z;
+            } f3;
+            struct
+            {
+                float x;
+                float y;
+                float z;
+                float w;
+            } f4;
+
+            struct
+            {
+                int x;
+            } i1;
+            struct
+            {
+                int x;
+                int y;
+            } i2;
+            struct
+            {
+                int x;
+                int y;
+                int z;
+            } i3;
+            struct
+            {
+                int x;
+                int y;
+                int z;
+                int w;
+            } i4;
+
+            struct
+            {
+                char value[224];
+            } path;
+        };
+    };
+
+
     /// Base class for material assets.
     class MaterialAsset : public Asset
     {
@@ -31,6 +97,20 @@ namespace GT
 
         /// Retrieves the size of the raw material data, in bytes.
         virtual unsigned int GetDataSizeInBytes() const = 0;
+
+
+        /// Retrieves the number of input variables.
+        virtual unsigned int GetInputCount() const;
+
+        /// Retrieves an input variable by it's count.
+        virtual bool GetInputByIndex(unsigned int index, MaterialVariable &variableOut);
+
+
+        /// Retrieves the number of properties.
+        virtual unsigned int GetPropertyCount() const;
+
+        /// Retrieves a property by it's count.
+        virtual bool GetPropertyByIndex(unsigned int index, MaterialVariable &variableOut);
     };
 }
 
--- source/Assets/MaterialAsset.cpp
+++ source/Assets/MaterialAsset.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2011 - 2015 David Reid. See included LICENCE file.
 
 #include <GTGameEngine/Assets/MaterialAsset.hpp>
+#include "../external/easy_mtl/easy_mtl.h"
 
 namespace GT
 {
@@ -17,5 +18,71 @@ namespace GT
     AssetClass MaterialAsset::GetClass() const
     {
         return AssetClass_Material;
+    }
+
+
+    unsigned int MaterialAsset::GetInputCount() const
+    {
+        easymtl_material material;
+        if (easymtl_initfromexisting_nocopy(&material, this->GetData(), this->GetDataSizeInBytes()))
+        {
+            return easymtl_getinputcount(&material);
+        }
+
+        return 0;
+    }
+
+    bool MaterialAsset::GetInputByIndex(unsigned int index, MaterialVariable &variableOut)
+    {
+        easymtl_material material;
+        if (easymtl_initfromexisting_nocopy(&material, this->GetData(), this->GetDataSizeInBytes()))
+        {
+            easymtl_input* pInput = easymtl_getinputbyindex(&material, index);
+            if (pInput != nullptr)
+            {
+                easymtl_identifier* pIdentifier = easymtl_getidentifier(&material, pInput->identifierIndex);
+                if (pIdentifier != nullptr)
+                {
+                    easymtl_strcpy(variableOut.name, 28, pIdentifier->name);
+                    variableOut.type = (GraphicsMaterialVariableType)pIdentifier->type;
+                    memcpy(variableOut.path.value, pInput->path.value, 224);
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    unsigned int MaterialAsset::GetPropertyCount() const
+    {
+        easymtl_material material;
+        if (easymtl_initfromexisting_nocopy(&material, this->GetData(), this->GetDataSizeInBytes()))
+        {
+            return easymtl_getpropertycount(&material);
+        }
+
+        return 0;
+    }
+
+    bool MaterialAsset::GetPropertyByIndex(unsigned int index, MaterialVariable &variableOut)
+    {
+        easymtl_material material;
+        if (easymtl_initfromexisting_nocopy(&material, this->GetData(), this->GetDataSizeInBytes()))
+        {
+            easymtl_property* pProperty = easymtl_getpropertybyindex(&material, index);
+            if (pProperty != nullptr)
+            {
+                easymtl_strcpy(variableOut.name, 28, pProperty->name);
+                variableOut.type = (GraphicsMaterialVariableType)pProperty->type;
+                memcpy(variableOut.path.value, pProperty->path.value, 224);
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
