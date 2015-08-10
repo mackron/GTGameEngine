9a15011a35c6004062025e50ab29d151ad46ba1d
David Reid <mackron@gmail.com>
2015-08-10 13:01:58 +1000

    Add the MeshBuilder class for helping build meshes.

---
+++ include/GTGameEngine/MeshBuilder.hpp
@@ -0,0 +1,121 @@
+// Copyright (C) 2011 - 2015 David Reid. See included LICENCE.
+
+#ifndef __GT_MeshBuilder_hpp_
+#define __GT_MeshBuilder_hpp_
+
+#include <GTLib/Vector.hpp>
+
+namespace GT
+{
+    /// Class used for building meshes.
+    ///
+    /// The way this class works is it emits primitives. In triangle mode, every 3 emits will make up the triangle.
+    class MeshBuilder
+    {
+    public:
+
+        /// Constructor.
+        MeshBuilder(size_t vertexSizeInFloats, bool checkDuplicatesOnEmit = true);
+
+        /// Destructor.
+        virtual ~MeshBuilder();
+
+
+        /// Emits a vertex.
+        ///
+        /// @param vertexData [in] A pointer to the vertex data. The size of this buffer is determined by the format specified in the constructor.
+        ///
+        /// @remarks
+        ///     If an identical vertex has already been emitted, that one will be used instead of adding a duplicate vertex.
+        void EmitVertex(const float* vertexData);
+
+        /// Merges another mesh builder into this one.
+        ///
+        /// @param other [in] The other mesh builder to add to this one.
+        ///
+        /// @remarks
+        ///     Both mesh builders MUST be in the same format.
+        void Merge(const MeshBuilder &other);
+
+
+        /// Finds the index of the given vertex.
+        ///
+        /// @param vertexData [in ] A pointer to the vertex data to search for.
+        /// @param indexOut   [out] A reference to the variable that will receive the index, if found.
+        ///
+        /// @return
+        ///     True if the vertex was found; false otherwise.
+        bool Find(const float* vertexData, unsigned int &indexOut);
+
+
+        /// Compares two vertex data buffers for equality.
+        ///
+        /// @param vertexData0 [in] The first buffer to compare against.
+        /// @param vertexData1 [in] The second buffer to compare against.
+        ///
+        /// @return True if the vertices are equal; false otherwise.
+        bool Equal(const float* vertexData0, const float* vertexData1);
+
+
+        /// Retrieves a pointer to the buffer containing the vertex data.
+              float* GetVertexData()       { return m_vertexBuffer.buffer; }
+        const float* GetVertexData() const { return m_vertexBuffer.buffer; }
+
+        /// Retrieves the number of vertices in the vertex buffer.
+        size_t GetVertexCount() const { return m_vertexBuffer.count / m_vertexSizeInFloats; }
+
+
+        /// Retrieves a pointer to the buffer containing the indices.
+              unsigned int* GetIndexData()       { return m_indexBuffer.buffer; }
+        const unsigned int* GetIndexData() const { return m_indexBuffer.buffer; }
+
+        /// Retrieves the number of indices in the vertex buffer.
+        size_t GetIndexCount() const { return m_indexBuffer.count; }
+
+
+        /// Clears the mesh builder.
+        void Clear();
+
+
+        /// Retrieves the size of each vertex in floats.
+        ///
+        /// @remarks
+        ///     The return value will always be the same as the value passed to the constructor.
+        size_t GetVertexSizeInFloats() const { return m_vertexSizeInFloats; }
+
+
+    protected:
+
+        /// The number of floats making up each vertex.
+        size_t m_vertexSizeInFloats;
+
+        /// The buffer containing the vertex data.
+        GTLib::Vector<float> m_vertexBuffer;
+
+        /// The buffer containing the indices.
+        GTLib::Vector<unsigned int> m_indexBuffer;
+
+        /// Whether or not the mesh builder should check for duplicate vertices. Defaults to true.
+        bool m_checkDuplicatesOnEmit;
+    };
+
+
+
+    /// Mesh builder class optimized for the common P3T2N3 texture format.
+    class MeshBuilderP3T2N3 : public MeshBuilder
+    {
+    public:
+
+        /// Default constructor.
+        MeshBuilderP3T2N3(bool checkDuplicatesOnEmit = true);
+
+        /// Destructor.
+        ~MeshBuilderP3T2N3();
+
+
+        /// Emits a vertex with a position, texture coordinate and normal.
+        void EmitVertex(float positionX, float positionY, float positionZ, float texCoordX, float texCoordY, float normalX, float normalY, float normalZ);
+    };
+}
+
+#endif
---
+++ source/MeshBuilder.cpp
@@ -0,0 +1,105 @@
+// Copyright (C) 2011 - 2015 David Reid. See included LICENCE.
+
+#include <GTGameEngine/MeshBuilder.hpp>
+
+namespace GT
+{
+    MeshBuilder::MeshBuilder(size_t vertexSizeInFloats, bool checkDuplicatesOnEmit)
+        : m_vertexSizeInFloats(vertexSizeInFloats), m_vertexBuffer(), m_indexBuffer(), m_checkDuplicatesOnEmit(checkDuplicatesOnEmit)
+    {
+    }
+
+    MeshBuilder::~MeshBuilder()
+    {
+    }
+
+    void MeshBuilder::EmitVertex(const float* vertexData)
+    {
+        unsigned int index;
+        if (!m_checkDuplicatesOnEmit || !this->Find(vertexData, index))
+        {
+            index = static_cast<unsigned int>(m_vertexBuffer.count) / m_vertexSizeInFloats;
+
+            // Here is where we append the input vertex data to the mesh builder's vertex data buffer.
+            for (auto i = 0U; i < m_vertexSizeInFloats; ++i)
+            {
+                m_vertexBuffer.PushBack(vertexData[i]);
+            }
+        }
+
+        // Now we just add the index to the end.
+        m_indexBuffer.PushBack(index);
+    }
+
+    void MeshBuilder::Merge(const MeshBuilder &other)
+    {
+        for (size_t iIndex = 0; iIndex < other.GetIndexCount(); ++iIndex)
+        {
+            unsigned int index = other.GetIndexData()[iIndex];
+            auto vertex        = other.GetVertexData() + (other.GetVertexSizeInFloats() * index);
+
+            this->EmitVertex(vertex);
+        }
+    }
+
+    bool MeshBuilder::Find(const float* vertexData, unsigned int &indexOut)
+    {
+        size_t vertexCount = m_vertexBuffer.count / m_vertexSizeInFloats;
+
+        for (size_t i = 0; i < vertexCount; ++i)
+        {
+            if (this->Equal(m_vertexBuffer.buffer + (i * m_vertexSizeInFloats), vertexData))
+            {
+                indexOut = static_cast<unsigned int>(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool MeshBuilder::Equal(const float* vertexData0, const float* vertexData1)
+    {
+        for (size_t i = 0; i < m_vertexSizeInFloats; ++i)
+        {
+            if (vertexData0[i] != vertexData1[i])
+            {
+                return false;
+            }
+        }
+
+        // If we make it here, it means we are equal.
+        return true;
+    }
+
+    void MeshBuilder::Clear()
+    {
+        m_vertexBuffer.Clear();
+        m_indexBuffer.Clear();
+    }
+
+
+
+
+    /// MeshBuilderP3T2N3
+    MeshBuilderP3T2N3::MeshBuilderP3T2N3(bool checkDuplicatesOnEmit)
+        : MeshBuilder(8, checkDuplicatesOnEmit)
+    {
+    }
+
+    MeshBuilderP3T2N3::~MeshBuilderP3T2N3()
+    {
+    }
+
+    void MeshBuilderP3T2N3::EmitVertex(float positionX, float positionY, float positionZ, float texCoordX, float texCoordY, float normalX, float normalY, float normalZ)
+    {
+        float data[8] =
+        {
+            positionX, positionY, positionZ,
+            texCoordX, texCoordY,
+            normalX,   normalY,   normalZ
+        };
+
+        MeshBuilder::EmitVertex(data);
+    }
+}
