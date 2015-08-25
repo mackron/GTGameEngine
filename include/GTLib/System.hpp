decef6a8a49eac5852c03033567296a106b2d77d
David Reid <mackron@gmail.com>
2015-08-25 23:23:13 +1000

    Remove some bad code for retrieving system directories.

--- include/GTLib/System.hpp
+++ include/GTLib/System.hpp
@@ -4,36 +4,15 @@
 #define GTLib_System
 
 #include <cstddef>
-#include "Vector.hpp"
 
 namespace GTLib
 {
     namespace System
     {
-        /// Static class for retrieving system directories. In some cases, such as fonts, there will multiple directories. In these cases the retrieval
-        /// functions will return a Vector containing the directory strings.
-        class Directories
-        {
-        public:
-
-            /**
-            *   \brief  Retrieves a pointer to the string containing the home directory path.
-            */
-            static const char* Home();
-
-            /// Retrieves the list of root-level font directories. Note that this does NOT include sub-directories. For example, on Linux it will include
-            /// "/usr/share/fonts", but will not include "/usr/share/fonts/freetype".
-            static Vector<const char*> & Fonts();
-        };
-
-        /**
-        *   \brief  Retrieves the number of logical CPU's attached to the machine.
-        */
+        /// Retrieves the number of logical CPU's attached to the machine.
         unsigned int GetCPUCount();
 
-        /**
-        *   \brief  Retrieves the page size.
-        */
+        /// Retrieves the page size.
         size_t GetPageSize();
 
 
