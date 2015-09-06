580f9108d978ef6ed6672343d7ab64ed3f96dc65
David Reid <mackron@gmail.com>
2015-09-06 20:43:57 +1000

    More work on the default game state.

--- include/GTGameEngine/DefaultGameState.hpp
+++ include/GTGameEngine/DefaultGameState.hpp
@@ -6,6 +6,12 @@
 #include "GameState.hpp"
 #include "EngineContext.hpp"
 #include "GameContext.hpp"
+#include "ConfigFile.hpp"
+#include "Graphics/GraphicsWorldGUIRendererAndResourceManager.hpp"
+#include "Graphics/DefaultGraphicsWorld.hpp"
+#include "Scene/Scene.hpp"
+#include "Scene/DefaultSceneCallback.hpp"
+#include <GTLib/Vector.hpp>
 
 namespace GT
 {
@@ -20,10 +26,14 @@ namespace GT
         DefaultGameState();
 
         /// Destructor.
-        ~DefaultGameState();
+        virtual ~DefaultGameState();
 
 
         /// Starts up the game.
+        ///
+        /// @remarks
+        ///     This will initialize the engine and game context's, load the config files and create the primary game window. Create
+        ///     secondary game windows with CreateSecondaryGameWindow().
         bool Startup(int argc, char** argv);
 
         /// Shuts down the game.
@@ -32,15 +42,38 @@ namespace GT
         /// Begins the game loop and returns the exit code.
         ///
         /// @remarks
-        ///     Upon returning, this does NOT shut down the game with Shutdown() - that must be done manually.
+        ///     Upon returning, this does NOT shut down the game with Shutdown() - that must be done manually, or left to the destructor.
         int Run();
 
 
         /// Retrieves a pointer to the engine context.
-        EngineContext* GetEngineContext() { return m_pEngineContext; }
+        EngineContext & GetEngineContext() { return m_engine; }
 
         /// Retrieves a reference to the game context.
-        GameContext* GetGameContext() { return m_pGameContext; }
+        GameContext & GetGameContext() { return m_game; }
+
+        /// Retrieves a reference to the object representing the config file.
+        ConfigFile & GetConfig() { return m_config; }
+
+
+        /// Retrieves a handle to the primary window.
+        HWindow GetPrimaryWindow() const;
+
+        /// Creates a secondary game window.
+        ///
+        /// @remarks
+        ///     The primary window is created automatically by Startup().
+        HWindow CreateSecondaryWindow(int monitor, int xPos, int yPos, unsigned int width, unsigned int height, bool fullscreen);
+
+
+        /// Enables the editor toggle shortcut (enabled by default).
+        void EnableEditorShortcut();
+
+        /// Disables the editor toggle shortcut.
+        void DisableEditorShortcut();
+
+        /// Determines whether or not the editor shortcut is enabled.
+        bool IsEditorShortcutEnabled() const;
 
 
 
@@ -60,42 +93,190 @@ namespace GT
 
         /// Called when the game is being stepped/updated.
         ///
-        /// @param dt [in] The in seconds since the previous frame.
+        /// @param dt [in] The time in seconds since the previous frame.
         virtual void OnStep(double dt);
+
+        /// Called when the game is being stepped/updated while in a paused state.
+        ///
+        /// @param dt [in] The time in seconds since the previous frame.
+        virtual void OnStepWhilePaused(double dt);
+
+        /// Called after the game has been paused.
+        virtual void OnPause();
+
+        /// Called after the game has resumed after being paused.
+        virtual void OnResume();
+
+        /// Called when the mouse is moved while over the given window.
+        virtual void OnMouseMove(HWindow hWindow, int xPos, int yPos);
+        
+        /// Called when the mouse enters the given window.
+        virtual void OnMouseEnter(HWindow hWindow);
+        
+        /// Called when the mouse leaves the given window.
+        virtual void OnMouseLeave(HWindow hWindow);
+        
+        /// Called when a mouse button is pressed while over the given window.
+        virtual void OnMouseButtonPressed(HWindow hWindow, int button, int xPos, int yPos);
+        
+        /// Called when a mouse button is released while over the given window.
+        virtual void OnMouseButtonReleased(HWindow hWindow, int button, int xPos, int yPos);
+        
+        /// Called when a mouse button is double-clicked while over the given window.
+        virtual void OnMouseButtonDoubleClicked(HWindow hWindow, int button, int xPos, int yPos);
+        
+        /// Called when the mouse wheel is turned while over the given window.
+        virtual void OnMouseWheel(HWindow hWindow, int delta, int xPos, int yPos);
+        
+        /// Called when a key is pressed while the given window has the keyboard focus.
+        virtual void OnKeyPressed(HWindow hWindow, GTLib::Key key);
+        
+        /// Called when a key is released while the given window has the keyboard focus.
+        virtual void OnKeyReleased(HWindow hWindow, GTLib::Key key);
+        
+        /// Called when the given window is resized.
+        virtual void OnWindowResized(HWindow hWindow, unsigned int width, unsigned int height);
+
+
 
 
 
         ///////////////////////////////////////////
-        // Inherited from GT::GameState
+        // Inherited from GameState
 
-        void Step(GT::GameContext &gameContext, double deltaTimeInSeconds);
-        bool OnStarting(GT::GameContext &gameContext, const GTLib::CommandLine &commandLine);
-        void OnClosing(GT::GameContext &gameContext);
-    #if 0
-        unsigned int GetWindowCount(GT::GameContext &gameContext) const;
-        GT::HWindow GetWindowByIndex(GT::GameContext &gameContext, unsigned int index);
+        /// @copydoc GameState::GetWindowCount()
+        unsigned int GetWindowCount(GameContext &gameContext) const;
+
+        /// @copydoc GameState::GetWindowByIndex()
+        HWindow GetWindowByIndex(GameContext &gameContext, unsigned int index);
+
+        /// @copydoc GameState::Step()
+        void Step(GameContext &gameContext, double deltaTimeInSeconds);
+
+        /// @copydoc GameState::OnStarting()
+        bool OnStarting(GameContext &gameContext, const GTLib::CommandLine &commandLine);
+
+        /// @copydoc GameState::OnClosing()
+        void OnClosing(GameContext &gameContext);
+
+        /// @copydoc GameState::OnPause()
         void OnPause(GT::GameContext &gameContext);
+        
+        /// @copydoc GameState::OnResume()
         void OnResume(GT::GameContext &gameContext);
+        
+        /// @copydoc GameState::OnMouseMove()
         void OnMouseMove(GT::GameContext &gameContext, GT::HWindow hWindow, int xPos, int yPos);
+        
+        /// @copydoc GameState::OnMouseEnter()
         void OnMouseEnter(GT::GameContext &gameContext, GT::HWindow hWindow);
+        
+        /// @copydoc GameState::OnMouseLeave()
         void OnMouseLeave(GT::GameContext &gameContext, GT::HWindow hWindow);
+        
+        /// @copydoc GameState::OnMouseButtonPressed()
         void OnMouseButtonPressed(GT::GameContext &gameContext, GT::HWindow hWindow, int button, int xPos, int yPos);
+        
+        /// @copydoc GameState::OnMouseButtonReleased()
         void OnMouseButtonReleased(GT::GameContext &gameContext, GT::HWindow hWindow, int button, int xPos, int yPos);
+        
+        /// @copydoc GameState::OnMouseButtonDoubleClicked()
         void OnMouseButtonDoubleClicked(GT::GameContext &gameContext, GT::HWindow hWindow, int button, int xPos, int yPos);
+        
+        /// @copydoc GameState::OnMouseWheel()
         void OnMouseWheel(GT::GameContext &gameContext, GT::HWindow hWindow, int delta, int xPos, int yPos);
+        
+        /// @copydoc GameState::OnKeyPressed()
         void OnKeyPressed(GT::GameContext &gameContext, GT::HWindow hWindow, GTLib::Key key);
+        
+        /// @copydoc GameState::OnKeyReleased()
         void OnKeyReleased(GT::GameContext &gameContext, GT::HWindow hWindow, GTLib::Key key);
+        
+        /// @copydoc GameState::OnWindowResized()
         void OnWindowResized(GT::GameContext &gameContext, GT::HWindow hWindow, unsigned int width, unsigned int height);
-    #endif
 
 
     private:
 
-        /// A pointer to the engine context. This is created in Startup()
-        EngineContext* m_pEngineContext;
+        /// Helper for loading the config.
+        void LoadConfig(const GTLib::CommandLine &commandLine);
 
-        /// A pointer to the game context. This is created in Startup()
-        GameContext* m_pGameContext;
+        /// Initializes the graphics sub-system.
+        bool InitializeGraphics();
+
+        /// Creates the primary game window based on the config file.
+        bool CreatePrimaryWindow();
+
+        /// Helper for creating a primary or secondary window.
+        HWindow CreateGameWindow(const char* title, int monitor, int xPos, int yPos, unsigned int width, unsigned int height, bool fullscreen);
+
+        /// Helper for deleting every game window.
+        void DeleteAllGameWindows();
+
+
+    private:
+
+        /// Data associated with each window.
+        struct WindowData
+        {
+            /// The window.
+            HWindow hWindow;
+            
+            /// The window's render target.
+            HGraphicsRenderTarget hRT;
+
+            /// The window's GUI surface.
+            HGUISurface hGUISurface;
+        };
+
+
+        /// A reference to the engine context. This is initialized in Startup()
+        EngineContext m_engine;
+
+        /// A pointer to the game context. This is initialized in Startup()
+        GameContext m_game;
+
+        /// The config file. Config files are loaded by LoadConfig(), which is called by OnStarting()
+        ConfigFile m_config; 
+
+
+        /// The GUI renderer.
+        GraphicsWorldGUIRendererAndResourceManager m_guiRendererAndResourceManager;
+
+        /// The GUI font manager. For now we're using GDI, but later on we'll make once specific to GraphicsWorld which will be better optimized.
+        GUIFontManager_GDI m_guiFontManager;
+
+        /// The GUI.
+        GUIContext m_gui;
+
+
+        /// The graphics world for drawing the current scene.
+        DefaultGraphicsWorld m_graphicsWorld;
+
+        /// The graphics asset resource manager.
+        GraphicsAssetResourceManager m_graphicsAssetResourceManager;
+
+
+        /// The scene callback.
+        DefaultSceneCallback m_sceneCallback;
+
+        /// The actual scene.
+        Scene m_scene;
+
+
+        /// The list of windows. The first window in this list is always the primary window.
+        GTLib::Vector<WindowData> m_windows;
+
+
+        /// Flags.
+        uint32_t m_flags;
+
+
+        ////////////////////////////////////////
+        // Flags
+
+        static const uint32_t Flag_IsEditorShortcutDisabled = (1 << 0);
+        static const uint32_t Flag_IsShiftKeyDown           = (1 << 1);       // For opening the editor with Shift+Tab.
 
 
     private:    // No copying.
--- source/DefaultGameState.cpp
+++ source/DefaultGameState.cpp
@@ -6,7 +6,12 @@ namespace GT
 {
     DefaultGameState::DefaultGameState()
         : GameState(),
-          m_pEngineContext(nullptr), m_pGameContext(nullptr)
+          m_engine(), m_game(m_engine, *this),
+          m_config(),
+          m_guiRendererAndResourceManager(), m_guiFontManager(FontOption_NoClearType), m_gui(&m_guiRendererAndResourceManager, &m_guiFontManager, &m_guiRendererAndResourceManager),
+          m_graphicsWorld(m_gui), m_graphicsAssetResourceManager(m_graphicsWorld, m_engine.GetAssetLibrary()),
+          m_sceneCallback(), m_scene(m_sceneCallback),
+          m_flags(0)
     {
     }
 
@@ -19,47 +24,72 @@ namespace GT
 
     bool DefaultGameState::Startup(int argc, char** argv)
     {
-        if (m_pEngineContext == nullptr)
+        if (m_engine.Startup(argc, argv))
         {
-            m_pEngineContext = new GT::EngineContext();
-            if (GT::Succeeded(m_pEngineContext->Startup(argc, argv)))
+            if (m_game.Startup())
             {
-                m_pGameContext = new GT::GameContext(*m_pEngineContext, *this);
-                if (GT::Succeeded(m_pGameContext->Startup()))
-                {
-                    return true;
-                }
-                else
-                {
-                    this->Shutdown();
-                }
+                return true;
             }
             else
             {
-                this->Shutdown();
+                // Failed to initialize the game. We want to shut down the engine and fall through to return false.
+                m_engine.Shutdown();
             }
         }
 
-        return m_pEngineContext != nullptr;
+        return false;
     }
 
     void DefaultGameState::Shutdown()
     {
-        if (m_pGameContext != nullptr) {
-            delete m_pGameContext;
-            m_pGameContext = nullptr;
-        }
+        m_graphicsWorld.Shutdown();
 
-        if (m_pEngineContext != nullptr) {
-            delete m_pEngineContext;
-            m_pEngineContext = nullptr;
-        }
+
+        // Shut down the game before the engine.
+        m_game.Shutdown();
+
+        // Shut down the engine last to ensure that anything that depends on the engine has been shut down beforehand.
+        m_engine.Shutdown();
     }
 
     int DefaultGameState::Run()
     {
-        return m_pGameContext->Run();
+        return m_game.Run();
     }
+
+
+    HWindow DefaultGameState::GetPrimaryWindow() const
+    {
+        if (m_windows.GetCount() > 0)
+        {
+            return m_windows[0].hWindow;
+        }
+
+        return NULL;
+    }
+
+    HWindow DefaultGameState::CreateSecondaryWindow(int monitor, int xPos, int yPos, unsigned int width, unsigned int height, bool fullscreen)
+    {
+        return this->CreateGameWindow("", monitor, xPos, yPos, width, height, fullscreen);
+    }
+
+
+
+    void DefaultGameState::EnableEditorShortcut()
+    {
+        m_flags &= ~Flag_IsEditorShortcutDisabled;
+    }
+
+    void DefaultGameState::DisableEditorShortcut()
+    {
+        m_flags |= Flag_IsEditorShortcutDisabled;
+    }
+
+    bool DefaultGameState::IsEditorShortcutEnabled() const
+    {
+        return (m_flags & Flag_IsEditorShortcutDisabled) == 0;
+    }
+
 
 
 
@@ -79,6 +109,58 @@ namespace GT
     {
     }
 
+    void DefaultGameState::OnStepWhilePaused(double)
+    {
+    }
+
+    void DefaultGameState::OnPause()
+    {
+    }
+
+    void DefaultGameState::OnResume()
+    {
+    }
+
+    void DefaultGameState::OnMouseMove(HWindow, int, int)
+    {
+    }
+
+    void DefaultGameState::OnMouseEnter(HWindow)
+    {
+    }
+
+    void DefaultGameState::OnMouseLeave(HWindow)
+    {
+    }
+
+    void DefaultGameState::OnMouseButtonPressed(HWindow, int, int, int)
+    {
+    }
+
+    void DefaultGameState::OnMouseButtonReleased(HWindow, int, int, int)
+    {
+    }
+
+    void DefaultGameState::OnMouseButtonDoubleClicked(HWindow, int, int, int)
+    {
+    }
+
+    void DefaultGameState::OnMouseWheel(HWindow, int, int, int)
+    {
+    }
+
+    void DefaultGameState::OnKeyPressed(HWindow, GTLib::Key)
+    {
+    }
+
+    void DefaultGameState::OnKeyReleased(HWindow, GTLib::Key)
+    {
+    }
+
+    void DefaultGameState::OnWindowResized(HWindow, unsigned int, unsigned int)
+    {
+    }
+
 
 
 
@@ -86,13 +168,60 @@ namespace GT
     ///////////////////////////////////////////
     // Inherited from GT::GameState
 
+    unsigned int DefaultGameState::GetWindowCount(GT::GameContext &) const
+    {
+        return static_cast<unsigned int>(m_windows.GetCount());
+    }
+
+    HWindow DefaultGameState::GetWindowByIndex(GT::GameContext &, unsigned int index)
+    {
+        assert(index < m_windows.GetCount());
+        return m_windows[index].hWindow;
+    }
+
     void DefaultGameState::Step(GT::GameContext &, double deltaTimeInSeconds)
     {
-        this->OnStep(deltaTimeInSeconds);
+        if (!m_game.IsPaused())
+        {
+            // Step the scene before calling OnStep()
+            m_scene.Step(deltaTimeInSeconds);
+            this->OnStep(deltaTimeInSeconds);
+        }
+        else
+        {
+            this->OnStepWhilePaused(deltaTimeInSeconds);
+        }
+
+
+        // Render the world after updating.
+        m_graphicsWorld.ExecuteRenderingCommands();
     }
 
     bool DefaultGameState::OnStarting(GT::GameContext &, const GTLib::CommandLine &commandLine)
     {
+        // Load the config before anything else. It doesn't matter if this fails because we will just use default in that case.
+        this->LoadConfig(commandLine);
+
+        // Initialize the graphics sub-system before creating the window. We do it like this so we can have access to an initialized
+        // graphics world which is where we'll create the render target for the primary window.
+        if (!this->InitializeGraphics())
+        {
+            return false;
+        }
+
+        // We want to create and display the window as soon as possible so that the user is given feedback that the game is starting. In
+        // this case it is immediately after initializing the graphics system.
+        if (!this->CreatePrimaryWindow())
+        {
+            return false;
+        }
+
+
+        // Make sure the GUI's DPI settings are correct.
+        unsigned int baseDPIX;
+        unsigned int baseDPIY;
+        m_game.GetWindowManager().GetBaseDPI(baseDPIX, baseDPIY);
+        m_gui.SetBaseDPI(baseDPIX, baseDPIY);
 
 
         return this->OnStartup(commandLine);
@@ -101,5 +230,245 @@ namespace GT
     void DefaultGameState::OnClosing(GT::GameContext &)
     {
         this->OnShutdown();
+
+        // Delete every window after OnShutdown().
+        this->DeleteAllGameWindows();
+
+        // Shutdown the graphics system after deleting all of the game windows.
+        m_graphicsAssetResourceManager.Shutdown();
+        m_graphicsWorld.Shutdown();
+    }
+
+    void DefaultGameState::OnPause(GT::GameContext &)
+    {
+        this->OnPause();
+    }
+
+    void DefaultGameState::OnResume(GT::GameContext &)
+    {
+        this->OnResume();
+    }
+
+    void DefaultGameState::OnMouseMove(GT::GameContext &, GT::HWindow hWindow, int xPos, int yPos)
+    {
+        this->OnMouseMove(hWindow, xPos, yPos);
+    }
+
+    void DefaultGameState::OnMouseEnter(GT::GameContext &, GT::HWindow hWindow)
+    {
+        this->OnMouseEnter(hWindow);
+    }
+
+    void DefaultGameState::OnMouseLeave(GT::GameContext &, GT::HWindow hWindow)
+    {
+        this->OnMouseLeave(hWindow);
+    }
+
+    void DefaultGameState::OnMouseButtonPressed(GT::GameContext &, GT::HWindow hWindow, int button, int xPos, int yPos)
+    {
+        this->OnMouseButtonPressed(hWindow, button, xPos, yPos);
+    }
+
+    void DefaultGameState::OnMouseButtonReleased(GT::GameContext &, GT::HWindow hWindow, int button, int xPos, int yPos)
+    {
+        this->OnMouseButtonReleased(hWindow, button, xPos, yPos);
+    }
+
+    void DefaultGameState::OnMouseButtonDoubleClicked(GT::GameContext &, GT::HWindow hWindow, int button, int xPos, int yPos)
+    {
+        this->OnMouseButtonDoubleClicked(hWindow, button, xPos, yPos);
+    }
+
+    void DefaultGameState::OnMouseWheel(GT::GameContext &, GT::HWindow hWindow, int delta, int xPos, int yPos)
+    {
+        this->OnMouseWheel(hWindow, delta, xPos, yPos);
+    }
+
+    void DefaultGameState::OnKeyPressed(GT::GameContext &, GT::HWindow hWindow, GTLib::Key key)
+    {
+        if (this->IsEditorShortcutEnabled())
+        {
+            if (key == GTLib::Keys::Shift)
+            {
+                m_flags |= Flag_IsShiftKeyDown;
+            }
+
+            if (key == GTLib::Keys::Tab && (m_flags & Flag_IsShiftKeyDown) != 0)
+            {
+                m_game.ToggleEditor();
+                return;     // Return here - don't want the tab key to be passed to the game because have just toggled the editor.
+            }
+        }
+
+        this->OnKeyPressed(hWindow, key);
+    }
+
+    void DefaultGameState::OnKeyReleased(GT::GameContext &, GT::HWindow hWindow, GTLib::Key key)
+    {
+        if (this->IsEditorShortcutEnabled())
+        {
+            if (key == GTLib::Keys::Shift)
+            {
+                m_flags &= ~Flag_IsShiftKeyDown;;
+            }
+        }
+
+        this->OnKeyReleased(hWindow, key);
+    }
+
+    void DefaultGameState::OnWindowResized(GT::GameContext &, GT::HWindow hWindow, unsigned int width, unsigned int height)
+    {
+        this->OnWindowResized(hWindow, width, height);
+    }
+
+
+
+
+    //////////////////////////////////////////
+    // Private
+
+    void DefaultGameState::LoadConfig(const GTLib::CommandLine &commandLine)
+    {
+        if (m_config.Initialize())
+        {
+            // The first, and lowest-priority config file should be default one - config.ini. It doesn't matter if this fails to load because
+            // it will just fall back to defaults.
+            m_config.LoadFile("config.ini", m_game.GetEngineContext().GetFileSystem());
+
+
+            // If the application was a passed a "--config" variable, that needs to be loaded.
+            const char** ppConfigPathFromCmdLine = commandLine.GetArgument("config");
+            if (ppConfigPathFromCmdLine != nullptr)
+            {
+                while (ppConfigPathFromCmdLine[0] != nullptr)
+                {
+                    m_config.LoadFile(ppConfigPathFromCmdLine[0], m_game.GetEngineContext().GetFileSystem());
+                    ppConfigPathFromCmdLine += 1;
+                }
+            }
+        }
+    }
+
+    bool DefaultGameState::InitializeGraphics()
+    {
+        GraphicsAPI* pBestAPI = m_engine.GetBestGraphicsAPI();
+        if (pBestAPI != nullptr)
+        {
+            if (m_graphicsWorld.Startup(*pBestAPI))
+            {
+                return m_graphicsAssetResourceManager.Startup();
+            }
+        }
+
+        return false;
+    }
+
+    bool DefaultGameState::CreatePrimaryWindow()
+    {
+        // Read the config.
+        char title[256];
+        unsigned int resolutionX  = 1280;
+        unsigned int resolutionY  = 720;
+        bool fullscreen = false;
+        int monitor = GT_PRIMARY_MONITOR;
+
+        strcpy_s(title, 256, "GTGameEngine");
+        if (m_config.IsString("Title")) {
+            m_config.GetString("Title", title, sizeof(title));
+        }
+        if (m_config.IsNumber("ResolutionX")) {
+            resolutionX = static_cast<unsigned int>(m_config.GetInteger("ResolutionX"));
+        }
+        if (m_config.IsNumber("ResolutionY")) {
+            resolutionY = static_cast<unsigned int>(m_config.GetInteger("ResolutionY"));
+        }
+        if (m_config.IsBoolean("Fullscreen")) {
+            fullscreen = m_config.GetBoolean("Fullscreen");
+        }
+        if (m_config.IsNumber("Monitor")) {
+            monitor = m_config.GetInteger("Monitor");
+        }
+
+        return this->CreateGameWindow(title, monitor, 0, 0, resolutionX, resolutionY, fullscreen) != NULL;
+    }
+
+    HWindow DefaultGameState::CreateGameWindow(const char* title, int monitor, int xPos, int yPos, unsigned int width, unsigned int height, bool fullscreen)
+    {
+        GT::WindowManager &windowManager = m_game.GetWindowManager();
+
+        if (width == height && width == 0)
+        {
+            // Make the resolution the resolution of the specified monitor. We first try to get the resolution of the requested monitor, but if that fails we try the default monitor.
+            if (!windowManager.GetMonitorResolution(monitor, width, height))
+            {
+                // We failed to retrieve the resolution of the requested monitor, so switch to the primary monitor.
+                if (monitor != GT_PRIMARY_MONITOR && !windowManager.GetMonitorResolution(0, width, height))
+                {
+                    // We have failed to retrieve the resolution of the primary monitor. Switch to windowed mode with a resolution of 1280 x 720.
+                    width  = 1280;
+                    height = 720;
+                    fullscreen = false;
+                }
+
+                monitor = GT_PRIMARY_MONITOR;
+            }
+        }
+
+        GT::WindowType windowType = GT::WindowType::PrimaryWindow;
+        if (fullscreen)
+        {
+            windowType = GT::WindowType::PrimaryWindow_Borderless;
+        }
+
+
+        int monitorOriginX = 0;
+        int monitorOriginY = 0;
+        windowManager.GetMonitorOrigin(monitor, monitorOriginX, monitorOriginY);
+
+        WindowData window;
+        window.hWindow = m_game.CreateMainWindow(windowType, title, monitorOriginX + xPos, monitorOriginY + yPos, width, height);
+        if (window.hWindow == 0)
+        {
+            return false;
+        }
+
+        // Show the window ASAP.
+        m_game.ShowWindow(window.hWindow);
+
+        // We need to create a render 
+        window.hRT = m_graphicsWorld.CreateRenderTargetFromWindow(reinterpret_cast<HWND>(window.hWindow), 0);
+
+        // We need a GUI surface for the main window.
+        window.hGUISurface = m_gui.CreateSurface();
+        m_graphicsWorld.SetRenderTargetGUISurface(window.hRT, window.hGUISurface);
+        m_graphicsWorld.SetRenderTargetSurfaceSizeToRenderTarget(window.hRT);
+
+
+        // The surface's DPI needs to be set based on the DPI supplied by the operating system.
+        unsigned int dpiX;
+        unsigned int dpiY;
+        if (windowManager.GetWindowDPI(window.hWindow, dpiX, dpiY))
+        {
+            m_gui.SetDPI(window.hGUISurface, dpiX, dpiY);
+        }
+
+
+        m_windows.PushBack(window);
+
+        return window.hWindow;
+    }
+
+    void DefaultGameState::DeleteAllGameWindows()
+    {
+        for (size_t iWindow = 0; iWindow < m_windows.GetCount(); ++iWindow)
+        {
+            WindowData &window = m_windows[iWindow];
+
+            m_graphicsWorld.DeleteRenderTarget(window.hRT);
+            m_gui.DeleteSurface(window.hGUISurface);
+            m_game.DeleteWindow(window.hWindow);
+        }
+
+        m_windows.Clear();
     }
 }
