2fb2b643ea98527c718c6fa71323eeae85e15138
David Reid <mackron@gmail.com>
2015-09-27 12:41:09 +1000

    Minor styling improvement to menu separators.

--- source/Editor/Controls/EditorMenuSeparator.cpp
+++ source/Editor/Controls/EditorMenuSeparator.cpp
@@ -8,8 +8,10 @@ namespace GT
         : EditorControl(editor)
     {
         m_gui.SetElementParent(m_hRootElement, hParent);
-        m_gui.SetElementPadding(m_hRootElement, 4);
-        m_gui.SetElementHeight(m_hRootElement, 9U);
+        m_gui.SetElementPadding(m_hRootElement, 2);
+        m_gui.SetElementPaddingLeft(m_hRootElement, 8);
+        m_gui.SetElementPaddingRight(m_hRootElement, 8);
+        m_gui.SetElementHeight(m_hRootElement, 5U);
         m_gui.SetElementWidthRatio(m_hRootElement, 1);
         m_gui.SetElementBackgroundColorBoundary(m_hRootElement, BackgroundBoundary_Inner);
         m_gui.SetElementBackgroundColor(m_hRootElement, GTLib::Colour(0.3, 0.3, 0.3));
